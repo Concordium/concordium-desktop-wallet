4999b55b6953de061006c65b870d9d2a3583aed4
Hjort <shj@concordium.com>
2021-01-05 15:12:26 +0100

    Transfers now work with non-initial accounts

--- app/components/ConfirmTransfer.tsx
+++ app/components/ConfirmTransfer.tsx
@@ -1,34 +1,38 @@
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
-import { AccountTransaction, TransactionKind } from '../utils/types';
 import { useSelector } from 'react-redux';
-import { identitiesSelector } from '../features/IdentitySlice.ts';
+import { AccountTransaction, TransactionKind } from '../utils/types';
+import { identitiesSelector } from '../features/IdentitySlice';
 import { sendTransaction, getNextAccountNonce } from '../utils/client';
-import { serializeTransaction, getTransactionHash } from '../utils/transactionSerialization';
-import { makeSimpleTransfer } from '../utils/rustInterface.ts';
+import {
+    serializeTransaction,
+    getTransactionHash,
+} from '../utils/transactionSerialization';
 import LedgerComponent from './LedgerComponent';
 
 import locations from '../constants/transferLocations.json';
 
-
-function getIdentityId(identities,identityName) {
-    return identities.filter(identity => identity.name === identityName)[0].id; // TODO: Can we assume names are unique?
+function getIdentityId(identities, identityName) {
+    return identities.filter((identity) => identity.name === identityName)[0]
+        .id; // TODO: Can we assume names are unique?
 }
 
 async function ledgerSignTransfer(
     transaction,
+    accountNumber,
     identityId,
     ledger,
     setSerializedTransaction,
     setTransactionHash
 ) {
-    const path = [0, 0, identityId, 2, 0, 0];
+    const path = [0, 0, identityId, 2, accountNumber, 0];
     const signature = await ledger.signTransfer(transaction, path);
-    console.log(Buffer.from(serializeTransaction(transaction, () => [signature])).toString('hex'))
     setSerializedTransaction(
         serializeTransaction(transaction, () => [signature])
     );
-    setTransactionHash(getTransactionHash(transaction,() => [signature]).toString('hex'))
+    setTransactionHash(
+        getTransactionHash(transaction, () => [signature]).toString('hex')
+    );
     return signature;
 }
 
@@ -37,13 +41,13 @@ function toMicroUnits(amount) {
 }
 
 async function createTransaction(fromAddress, amount, recipient) {
-    const nonceJSON =  await getNextAccountNonce(fromAddress);
-    const nonce = JSON.parse(nonceJSON.getValue()).nonce;
+    const nonceJSON = await getNextAccountNonce(fromAddress);
+    const { nonce } = JSON.parse(nonceJSON.getValue());
     const transferTransaction: AccountTransaction = {
         sender: fromAddress,
         nonce,
         energyAmount: 200, // TODO: Does this need to be set by the user?
-        expiry: 18446744073709, // TODO: Don't hardcode?
+        expiry: 16446744073, // TODO: Don't hardcode?
         transactionKind: TransactionKind.Simple_transfer,
         payload: {
             toAddress: recipient.address,
@@ -70,7 +74,11 @@ function ConfirmTransferComponent({
     const estimatedFee = 1; // TODO calculate
 
     useEffect(() => {
-        createTransaction(account.address, amount, recipient).then(transferTransaction => setTransaction(transferTransaction))
+        createTransaction(account.address, amount, recipient)
+            .then((transferTransaction) => setTransaction(transferTransaction))
+            .catch((e) =>
+                console.log(`unable to create transaction due to : ${e.stack} `)
+            );
     }, [setTransaction, account, amount, recipient]);
 
     async function submit() {
@@ -103,6 +111,7 @@ function ConfirmTransferComponent({
                     ledgerCall={(ledger) =>
                         ledgerSignTransfer(
                             transaction,
+                            account.accountNumber,
                             getIdentityId(identities, account.identityName),
                             ledger,
                             setSerializedTransaction,
