86288191024020464962550f7a81fd18b779804f
Søren Bruus Zeppelin <sz@concordium.com>
2021-02-04 11:33:03 +0100

    CB-1089 add form validation to add address form

--- app/components/AddAddress.tsx
+++ app/components/AddAddress.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Form, Input, TextArea } from 'semantic-ui-react';
+import { Button, Form } from 'semantic-ui-react';
 import { useForm, Controller, SubmitHandler } from 'react-hook-form';
 import { AddressBookEntry } from '../utils/types';
 
@@ -9,9 +9,17 @@ interface Props {
     initialValues?: AddressBookEntry;
 }
 
+const NOTE_MAX_LENGTH = 255;
+
 function AddAddress({ close, submit, initialValues }: Props) {
-    const { control, handleSubmit } = useForm<AddressBookEntry>({
+    const {
+        handleSubmit,
+        errors,
+        formState,
+        control,
+    } = useForm<AddressBookEntry>({
         defaultValues: initialValues,
+        mode: 'onTouched',
     });
 
     const onSubmit: SubmitHandler<AddressBookEntry> = ({
@@ -25,42 +33,88 @@ function AddAddress({ close, submit, initialValues }: Props) {
 
     return (
         <Form onSubmit={handleSubmit(onSubmit)}>
-            <Form.Field>
-                <Controller
-                    name="name"
-                    control={control}
-                    rules={{ required: true }}
-                    render={(props) => (
-                        <Input {...props} placeholder="Enter recipient name" />
-                    )}
-                />
-            </Form.Field>
-            <Form.Field>
-                <Controller
-                    name="address"
-                    control={control}
-                    rules={{ required: true }}
-                    render={(props) => (
-                        <Input
-                            {...props}
+            <Controller
+                name="name"
+                control={control}
+                rules={{ required: 'Name required' }}
+                render={(p) => (
+                    <Form.Input
+                        {...p}
+                        placeholder="Enter recipient name"
+                        error={errors.name?.message}
+                    />
+                )}
+            />
+            <Controller
+                name="address"
+                control={control}
+                rules={{
+                    required: 'Address should be 50 characters',
+                    minLength: {
+                        value: 50,
+                        message: 'Address should be 50 characters',
+                    },
+                    maxLength: {
+                        value: 50,
+                        message: 'Address should be 50 characters',
+                    },
+                }}
+                render={(p) => (
+                    <div style={{ position: 'relative', marginBottom: '2em' }}>
+                        <Form.Input
+                            {...p}
                             placeholder="Paste the account address here"
+                            error={errors.address?.message}
                         />
-                    )}
-                />
-            </Form.Field>
-            <Form.Field>
-                <Controller
-                    name="note"
-                    control={control}
-                    rules={{ maxLength: 255 }}
-                    render={(props) => (
-                        <TextArea
-                            {...props}
+                        <div
+                            className="text-small text-right"
+                            style={{
+                                position: 'absolute',
+                                bottom: -5,
+                                right: 0,
+                                transform: 'translateY(100%)',
+                            }}
+                        >
+                            {(p.value as string)?.length ?? 0}
+                        </div>
+                    </div>
+                )}
+            />
+            <Controller
+                name="note"
+                control={control}
+                rules={{
+                    maxLength: {
+                        value: NOTE_MAX_LENGTH,
+                        message: 'Message cannot be longer than 255 characters',
+                    },
+                }}
+                render={(p) => (
+                    <div style={{ position: 'relative', marginBottom: '2em' }}>
+                        <Form.TextArea
+                            {...p}
+                            value={(p.value as string)?.substring(
+                                0,
+                                NOTE_MAX_LENGTH - 1
+                            )}
                             placeholder="You can add a note here"
+                            error={errors.note?.message}
                         />
-                    )}
-                />
-            </Form.Field>
+                        <div
+                            className="text-small text-right"
+                            style={{
+                                position: 'absolute',
+                                bottom: -5,
+                                right: 0,
+                                transform: 'translateY(100%)',
+                            }}
+                        >
+                            {(p.value as string)?.length ?? 0} /{' '}
+                            {NOTE_MAX_LENGTH}
+                        </div>
+                    </div>
+                )}
+            />
             <Button positive type="submit">
                 Submit
             </Button>
