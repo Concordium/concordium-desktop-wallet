305ce127c1ee24d7378448b03c55489655eca119
Hjort <shj@concordium.com>
2021-02-05 13:51:44 +0100

    moved startClient out of index

--- app/index.tsx
+++ app/index.tsx
@@ -6,39 +6,36 @@ import './styles/app.global.scss';
 import { updateSettings, findSetting } from './features/SettingsSlice';
 import { loadAllSettings } from './database/SettingsDao';
 import listenForTransactionStatus from './utils/TransactionStatusPoller';
-import { Settings } from './utils/types';
-import { setClientLocation } from './utils/client';
+import { Dispatch } from './utils/types';
+import { startClient } from './utils/client';
 import listenForIdentityStatus from './utils/IdentityStatusPoller';
 import listenForAccountStatus from './utils/AccountStatusPoller';
 
 const store = configuredStore();
-
-// Extracts node location from settings, and pass them to the grpc client.
-function startClient(settings: Settings[]) {
-    const nodeLocationSetting = findSetting('Node location', settings);
-    if (nodeLocationSetting) {
-        const { address, port } = JSON.parse(nodeLocationSetting.value);
-        setClientLocation(address, port);
-    } else {
-        throw new Error('unable to find Node location settings.');
-    }
-}
 
 /**
  * Loads settings from the database into the store.
  */
 async function loadSettingsIntoStore() {
     const settings = await loadAllSettings();
-    startClient(settings);
+    const nodeLocationSetting = findSetting('Node location', settings);
+    if (nodeLocationSetting) {
+        startClient(nodeLocationSetting);
+    } else {
+        throw new Error('unable to find Node location settings.');
+    }
     return store.dispatch(updateSettings(settings));
 }
-loadSettingsIntoStore();
 
-listenForAccountStatus(store.dispatch);
+async function onLoad(dispatch: Dispatch) {
+    await loadSettingsIntoStore();
 
-listenForIdentityStatus(store.dispatch);
+    listenForAccountStatus(dispatch);
+    listenForIdentityStatus(dispatch);
+    listenForTransactionStatus(dispatch);
+}
 
-listenForTransactionStatus(store.dispatch);
+onLoad(store.dispatch);
 
 const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer;
 
