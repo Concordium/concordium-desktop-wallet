2c6236d2f22cf5b96c8644cc4942773137ac5981
Søren Bruus Zeppelin <sz@concordium.com>
2021-02-08 16:09:17 +0100

    Fix migration issue

--- app/main.dev.ts
+++ app/main.dev.ts
@@ -24,16 +24,11 @@ import ipcCommands from './constants/ipcCommands.json';
  * an error prompt is displayed to the user, and the application is terminated.
  */
 async function migrate() {
-    let config: { migrationSource: WebpackMigrationSource };
-    if (process.env.NODE_ENV === 'production') {
-        config = {
-            migrationSource: new WebpackMigrationSource(
-                require.context('./database/migrations', false, /.ts$/)
-            ),
-        };
-    } else {
-        config = require('./database/knexfile.ts').development;
-    }
+    const config = {
+        migrationSource: new WebpackMigrationSource(
+            require.context('./database/migrations', false, /.ts$/)
+        ),
+    };
 
     knex()
         .then((db) => {
