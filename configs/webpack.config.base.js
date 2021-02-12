fe2f72ae1f4fb6deddebb44e9233203998e00eeb
Søren Bruus Zeppelin <sz@concordium.com>
2021-02-12 22:16:35 +0100

    babel to transpile optional chaining

--- configs/webpack.config.base.js
+++ configs/webpack.config.base.js
@@ -28,7 +28,17 @@ module.exports = {
                 test: /\.tsx?$/,
                 exclude: /node_modules/,
                 include: /app/,
-                use: [{ loader: 'ts-loader' }],
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            plugins: [
+                                '@babel/plugin-proposal-optional-chaining',
+                            ],
+                        },
+                    },
+                    'ts-loader',
+                ],
             },
         ],
     },
