b13fd6c02834c3c911a06c3b8811407835e3ae6b
Hjort <shj@concordium.com>
2021-02-15 14:51:25 +0100

    Moved functions that convert transactions between model to a separate file

--- app/features/TransactionSlice.ts
+++ app/features/TransactionSlice.ts
@@ -13,21 +13,16 @@ import {
     TransferTransaction,
     TransactionStatus,
     TransactionKindString,
-    OriginType,
     Account,
     AccountTransaction,
-    IncomingTransaction,
     Dispatch,
-    SimpleTransfer,
-    instanceOfSimpleTransfer,
     TransactionEvent,
-    ScheduledTransfer,
-    instanceOfScheduledTransfer,
 } from '../utils/types';
+import { attachNames } from '../utils/transactionHelpers';
 import {
-    attachNames,
-    getScheduledTransferAmount,
-} from '../utils/transactionHelpers';
+    convertIncomingTransaction,
+    convertAccountTransaction,
+} from '../utils/TransactionConverters';
 
 const transactionSlice = createSlice({
     name: 'transactions',
@@ -96,68 +91,6 @@ export async function decryptTransactions(
 }
 
 /**
- * We have to do it like this, because the data from the wallet proxy
- * doesn't contain the receiving address, except in the event string.
- */
-function getScheduleReceiver(transaction: IncomingTransaction) {
-    const event = transaction.details.events[0];
-    return event.slice(event.lastIndexOf(' ') + 1);
-}
-
-/*
- * Converts the given transaction into the structure, which is used in the database.
- */
-function convertIncomingTransaction(
-    transaction: IncomingTransaction
-): TransferTransaction {
-    let fromAddress = '';
-    if (transaction.details.transferSource) {
-        fromAddress = transaction.details.transferSource;
-    } else if (
-        transaction.origin.type === OriginType.Account &&
-        transaction.origin.address
-    ) {
-        fromAddress = transaction.origin.address;
-    }
-    let toAddress = '';
-    if (transaction.details.transferDestination) {
-        toAddress = transaction.details.transferDestination;
-    }
-    let encrypted;
-    if (transaction.encrypted) {
-        encrypted = JSON.stringify(transaction.encrypted);
-    }
-
-    if (
-        transaction.details.type === TransactionKindString.TransferWithSchedule
-    ) {
-        if (transaction.origin.type === OriginType.Account) {
-            toAddress = getScheduleReceiver(transaction);
-        }
-    }
-
-    return {
-        remote: true,
-        originType: transaction.origin.type,
-        transactionKind: transaction.details.type,
-        id: transaction.id,
-        blockHash: transaction.blockHash,
-        blockTime: transaction.blockTime,
-        total: transaction.total,
-        success: transaction.details.outcome === 'success',
-        transactionHash: transaction.transactionHash,
-        subtotal: transaction.subtotal,
-        cost: transaction.cost,
-        origin: JSON.stringify(transaction.origin),
-        details: JSON.stringify(transaction.details),
-        encrypted,
-        fromAddress,
-        toAddress,
-        status: TransactionStatus.Finalized,
-    };
-}
-
-/**
  * Determine whether the transaction affects unshielded balance.
  */
 function filterUnShieldedBalanceTransaction(transaction: TransferTransaction) {
@@ -213,62 +146,6 @@ export async function updateTransactions(account: Account) {
     if (transactions.length > 0) {
         await insertTransactions(transactions.map(convertIncomingTransaction));
     }
-}
-
-type TypeSpecific = Pick<
-    TransferTransaction,
-    'transactionKind' | 'total' | 'subtotal'
->;
-function convertSimpleTransfer(transaction: SimpleTransfer): TypeSpecific {
-    const amount = BigInt(transaction.payload.amount);
-    const estimatedTotal = amount + BigInt(transaction.energyAmount); // Fix this: convert from energy to cost
-
-    return {
-        transactionKind: TransactionKindString.Transfer,
-        total: (-estimatedTotal).toString(),
-        subtotal: (-amount).toString(),
-    };
-}
-
-function convertScheduledTransfer(
-    transaction: ScheduledTransfer
-): TypeSpecific {
-    const amount = getScheduledTransferAmount(transaction);
-    const estimatedTotal = amount + BigInt(transaction.energyAmount); // Fix this: convert from energy to cost
-
-    return {
-        transactionKind: TransactionKindString.TransferWithSchedule,
-        total: (-estimatedTotal).toString(),
-        subtotal: (-amount).toString(),
-    };
-}
-
-function convertAccountTransaction(
-    transaction: AccountTransaction,
-    hash: string
-): TransferTransaction {
-    let typeSpecific;
-    if (instanceOfSimpleTransfer(transaction)) {
-        typeSpecific = convertSimpleTransfer(transaction);
-    } else if (instanceOfScheduledTransfer(transaction)) {
-        typeSpecific = convertScheduledTransfer(transaction);
-    } else {
-        throw new Error('unsupported transaction type - please implement');
-    }
-
-    return {
-        id: -1,
-        blockHash: 'pending',
-        remote: false,
-        originType: OriginType.Self,
-        transactionHash: hash,
-        cost: transaction.energyAmount, // Fix this: convert from energy to cost
-        fromAddress: transaction.sender,
-        toAddress: transaction.payload.toAddress,
-        blockTime: (Date.now() / 1000).toString(), // Temporary value, unless it fails
-        status: TransactionStatus.Pending,
-        ...typeSpecific,
-    };
 }
 
 // Add a pending transaction to storage
---
+++ app/utils/TransactionConverters.ts
@@ -0,0 +1,122 @@
+import {
+    TransferTransaction,
+    TransactionStatus,
+    TransactionKindString,
+    OriginType,
+    AccountTransaction,
+    IncomingTransaction,
+    SimpleTransfer,
+    instanceOfSimpleTransfer,
+    ScheduledTransfer,
+    instanceOfScheduledTransfer,
+} from './types';
+import { getScheduledTransferAmount } from './transactionHelpers';
+
+/*
+ * Converts the given transaction into the structure, which is used in the database.
+ */
+export function convertIncomingTransaction(
+    transaction: IncomingTransaction
+): TransferTransaction {
+    let fromAddress = '';
+    if (transaction.details.transferSource) {
+        fromAddress = transaction.details.transferSource;
+    } else if (
+        transaction.origin.type === OriginType.Account &&
+        transaction.origin.address
+    ) {
+        fromAddress = transaction.origin.address;
+    }
+    let toAddress = '';
+    if (transaction.details.transferDestination) {
+        toAddress = transaction.details.transferDestination;
+    }
+    let encrypted;
+    if (transaction.encrypted) {
+        encrypted = JSON.stringify(transaction.encrypted);
+    }
+
+    return {
+        remote: true,
+        originType: transaction.origin.type,
+        transactionKind: transaction.details.type,
+        id: transaction.id,
+        blockHash: transaction.blockHash,
+        blockTime: transaction.blockTime,
+        total: transaction.total,
+        success: transaction.details.outcome === 'success',
+        transactionHash: transaction.transactionHash,
+        subtotal: transaction.subtotal,
+        cost: transaction.cost,
+        origin: JSON.stringify(transaction.origin),
+        details: JSON.stringify(transaction.details),
+        encrypted,
+        fromAddress,
+        toAddress,
+        status: TransactionStatus.Finalized,
+    };
+}
+
+// Return type for the functions for specific transaction types
+type TypeSpecific = Pick<
+    TransferTransaction,
+    'transactionKind' | 'total' | 'subtotal'
+>;
+
+// Convert the type specific fields of a Simple transfer for an Account Transaction.
+function convertSimpleTransfer(transaction: SimpleTransfer): TypeSpecific {
+    const amount = BigInt(transaction.payload.amount);
+    const estimatedTotal = amount + BigInt(transaction.energyAmount); // Fix this: convert from energy to cost
+
+    return {
+        transactionKind: TransactionKindString.Transfer,
+        total: (-estimatedTotal).toString(),
+        subtotal: (-amount).toString(),
+    };
+}
+
+// Convert the type specific fields of a Scheduled transfer for an Account Transaction.
+function convertScheduledTransfer(
+    transaction: ScheduledTransfer
+): TypeSpecific {
+    const amount = getScheduledTransferAmount(transaction);
+    const estimatedTotal = amount + BigInt(transaction.energyAmount); // Fix this: convert from energy to cost
+
+    return {
+        transactionKind: TransactionKindString.TransferWithSchedule,
+        total: (-estimatedTotal).toString(),
+        subtotal: (-amount).toString(),
+    };
+}
+
+/**
+ * Converts an Account Transaction, so that it fits local Transfer Transaction model and
+ * can be entered into the local database.
+ */
+export function convertAccountTransaction(
+    transaction: AccountTransaction,
+    hash: string
+): TransferTransaction {
+    let typeSpecific;
+    if (instanceOfSimpleTransfer(transaction)) {
+        typeSpecific = convertSimpleTransfer(transaction);
+    } else if (instanceOfScheduledTransfer(transaction)) {
+        typeSpecific = convertScheduledTransfer(transaction);
+    } else {
+        throw new Error('unsupported transaction type - please implement');
+    }
+
+    return {
+        id: -1,
+        blockHash: 'pending',
+        remote: false,
+        originType: OriginType.Self,
+        transactionHash: hash,
+        cost: transaction.energyAmount, // Fix this: convert from energy to cost
+        fromAddress: transaction.sender,
+        toAddress: transaction.payload.toAddress,
+        blockTime: (Date.now() / 1000).toString(), // Temporary value, unless it fails
+        status: TransactionStatus.Pending,
+        ...typeSpecific,
+    };
+}
