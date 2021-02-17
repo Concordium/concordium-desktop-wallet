2c7cf0418499e45a7dc3c622449ee32c073a38ca
Hjort <shj@concordium.com>
2021-02-17 14:52:10 +0100

    TransactionListElement uses current account's address instead of origin type

--- app/pages/Accounts/TransactionListElement.tsx
+++ app/pages/Accounts/TransactionListElement.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Grid } from 'semantic-ui-react';
 import { parseTime } from '../../utils/timeHelpers';
 import { getGTUSymbol, displayAsGTU } from '../../utils/gtu';
@@ -9,24 +10,23 @@ import {
     TransactionKindString,
 } from '../../utils/types';
 import SidedText from '../../components/SidedText';
+import { chosenAccountSelector } from '../../features/AccountSlice';
 
-function getName(transaction: TransferTransaction) {
-    switch (transaction.originType) {
-        case OriginType.Self:
-            if (transaction.toAddressName !== undefined) {
-                return transaction.toAddressName;
-            }
-            return transaction.toAddress.slice(0, 6);
-
-        case OriginType.Account:
-            if (transaction.fromAddressName !== undefined) {
-                return transaction.fromAddressName;
-            }
-            return transaction.fromAddress.slice(0, 6);
-
-        default:
-            return 'unknown';
+function getName(
+    transaction: TransferTransaction,
+    isOutgoingTransaction: boolean
+) {
+    if (isOutgoingTransaction) {
+        // Current Account is the sender
+        if (transaction.toAddressName !== undefined) {
+            return transaction.toAddressName;
+        }
+        return transaction.toAddress.slice(0, 6);
     }
+    if (transaction.fromAddressName !== undefined) {
+        return transaction.fromAddressName;
+    }
+    return transaction.fromAddress.slice(0, 6);
 }
 
 function buildOutgoingAmountStrings(
@@ -49,36 +49,40 @@ function buildIncomingAmountStrings(total: bigint) {
     };
 }
 
-function parseAmount(transaction: TransferTransaction) {
+function parseAmount(
+    transaction: TransferTransaction,
+    isOutgoingTransaction: boolean
+) {
     switch (transaction.originType) {
-        case OriginType.Self: {
-            const cost = BigInt(transaction.cost);
-            if (
-                transaction.transactionKind ===
-                TransactionKindString.EncryptedAmountTransfer
-            ) {
-                if (transaction.decryptedAmount) {
-                    const total = BigInt(transaction.decryptedAmount);
-                    return buildOutgoingAmountStrings(
-                        total,
-                        total - cost,
-                        cost
-                    );
-                }
-                return {
-                    amount: `${getGTUSymbol()} ?`,
-                    amountFormula: `${getGTUSymbol()} ? +${displayAsGTU(
-                        cost
-                    )} Fee`,
-                };
-            }
-            return buildOutgoingAmountStrings(
-                BigInt(transaction.total),
-                BigInt(transaction.subtotal),
-                cost
-            );
-        }
+        case OriginType.Self:
         case OriginType.Account:
+            if (isOutgoingTransaction) {
+                const cost = BigInt(transaction.cost || '0');
+                if (
+                    transaction.transactionKind ===
+                    TransactionKindString.EncryptedAmountTransfer
+                ) {
+                    if (transaction.decryptedAmount) {
+                        const total = BigInt(transaction.decryptedAmount);
+                        return buildOutgoingAmountStrings(
+                            total,
+                            total - cost,
+                            cost
+                        );
+                    }
+                    return {
+                        amount: `${getGTUSymbol()} ?`,
+                        amountFormula: `${getGTUSymbol()} ? +${displayAsGTU(
+                            cost
+                        )} Fee`,
+                    };
+                }
+                return buildOutgoingAmountStrings(
+                    BigInt(transaction.total),
+                    BigInt(transaction.subtotal),
+                    cost
+                );
+            }
             if (
                 transaction.transactionKind ===
                 TransactionKindString.EncryptedAmountTransfer
@@ -94,6 +98,7 @@ function parseAmount(transaction: TransferTransaction) {
                 };
             }
             return buildIncomingAmountStrings(BigInt(transaction.total));
+
         default:
             return {
                 amount: `${getGTUSymbol()} ?`,
@@ -134,9 +139,17 @@ interface Props {
  * Displays the given transaction basic information.
  */
 function TransactionListElement({ transaction }: Props): JSX.Element {
+    const account = useSelector(chosenAccountSelector);
+    if (!account) {
+        throw new Error('Unexpected missing chosen account');
+    }
+    const isOutgoingTransaction = transaction.fromAddress === account.address;
     const time = parseTime(transaction.blockTime);
-    const name = getName(transaction);
-    const { amount, amountFormula } = parseAmount(transaction);
+    const name = getName(transaction, isOutgoingTransaction);
+    const { amount, amountFormula } = parseAmount(
+        transaction,
+        isOutgoingTransaction
+    );
 
     return (
         <Grid container columns={2}>
