c00ee783aa9f58498878461373b50f7915038ed6
Hjort <shj@concordium.com>
2021-02-22 15:35:57 +0100

    Added Header to Accounts and Identities

--- app/pages/Accounts/AccountPage.tsx
+++ app/pages/Accounts/AccountPage.tsx
@@ -1,35 +1,57 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Grid } from 'semantic-ui-react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { push } from 'connected-react-router';
 import AccountList from './AccountList';
 import AccountView from './AccountView';
 import NoIdentities from '../../components/NoIdentities';
 import { accountsSelector } from '../../features/AccountSlice';
-import routes from '../../constants/routes.json';
+import PageHeader from '../../components/PageHeader';
 import BuildSchedule from './BuildSchedule';
+import routes from '../../constants/routes.json';
+
+import PlusIcon from '../../../resources/svg/plus.svg';
 
 export default function AccountsPage() {
+    const dispatch = useDispatch();
     const accounts = useSelector(accountsSelector);
+    let body;
     if (accounts.length === 0) {
-        return <NoIdentities />;
+        body = <NoIdentities />;
+    } else {
+        body = (
+            <Grid centered columns="equal" divided>
+                <Grid.Row>
+                    <Grid.Column>
+                        <AccountList />
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Switch>
+                            <Route
+                                path={routes.ACCOUNTS_SCHEDULED_TRANSFER}
+                                component={BuildSchedule}
+                            />
+                            <Route component={AccountView} />
+                        </Switch>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        );
     }
+
     return (
-        <Grid centered columns="equal" divided>
-            <Grid.Row>
-                <Grid.Column>
-                    <AccountList />
-                </Grid.Column>
-                <Grid.Column>
-                    <Switch>
-                        <Route
-                            path={routes.ACCOUNTS_SCHEDULED_TRANSFER}
-                            component={BuildSchedule}
-                        />
-                        <Route component={AccountView} />
-                    </Switch>
-                </Grid.Column>
-            </Grid.Row>
-        </Grid>
+        <>
+            <PageHeader>
+                <h1>Accounts</h1>
+                <PageHeader.Button
+                    align="right"
+                    onClick={() => dispatch(push(routes.ACCOUNTCREATION))}
+                >
+                    <PlusIcon />
+                </PageHeader.Button>
+            </PageHeader>
+            {body}
+        </>
     );
 }
--- app/pages/Identities/IdentityPage.tsx
+++ app/pages/Identities/IdentityPage.tsx
@@ -1,27 +1,48 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { push } from 'connected-react-router';
 import IdentityList from './IdentityList';
 import IdentityView from './IdentityView';
 import NoIdentities from '../../components/NoIdentities';
 import { identitiesSelector } from '../../features/IdentitySlice';
+import PageHeader from '../../components/PageHeader';
+import routes from '../../constants/routes.json';
+
+import PlusIcon from '../../../resources/svg/plus.svg';
 
 export default function IdentityPage() {
+    const dispatch = useDispatch();
     const identities = useSelector(identitiesSelector);
+    let body;
     if (identities.length === 0) {
-        return <NoIdentities />;
+        body = <NoIdentities />;
+    } else {
+        body = (
+            <Grid centered columns="equal" divided>
+                <Grid.Row>
+                    <Grid.Column>
+                        <IdentityList />
+                    </Grid.Column>
+                    <Grid.Column>
+                        <IdentityView />
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        );
     }
-
     return (
-        <Grid centered columns="equal" divided>
-            <Grid.Row>
-                <Grid.Column>
-                    <IdentityList />
-                </Grid.Column>
-                <Grid.Column>
-                    <IdentityView />
-                </Grid.Column>
-            </Grid.Row>
-        </Grid>
+        <>
+            <PageHeader>
+                <h1>Identities</h1>
+                <PageHeader.Button
+                    align="right"
+                    onClick={() => dispatch(push(routes.IDENTITYISSUANCE))}
+                >
+                    <PlusIcon />
+                </PageHeader.Button>
+            </PageHeader>
+            {body}
+        </>
     );
 }
