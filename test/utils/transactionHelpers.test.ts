36e83395e989b8438d0050dcf4a1561f17112093
Hjort <shj@concordium.com>
2021-02-22 11:41:11 +0100

    Renamed function in transactionHelper tests to make them work

--- test/utils/transactionHelpers.test.ts
+++ test/utils/transactionHelpers.test.ts
@@ -1,35 +1,40 @@
-import { createSchedule } from '../../app/utils/transactionHelpers';
+import { createRegularIntervalSchedule } from '../../app/utils/transactionHelpers';
 
-test('createSchedule release amounts should sum to input amount', () => {
+test('createRegularIntervalSchedule release amounts should sum to input amount', () => {
     const totalAmount = 100n;
     const releases = 7;
-    const schedule = createSchedule(totalAmount, releases, 0, 0);
+    const schedule = createRegularIntervalSchedule(totalAmount, releases, 0, 0);
     expect(
         schedule.reduce((acc, { amount }) => acc + BigInt(amount), 0n)
     ).toEqual(totalAmount);
 });
 
-test('createSchedule should put remainder onto last release', () => {
+test('createRegularIntervalSchedule should put remainder onto last release', () => {
     const amount = 100n;
     const releases = 7;
-    const schedule = createSchedule(amount, releases, 0, 0);
+    const schedule = createRegularIntervalSchedule(amount, releases, 0, 0);
     expect(BigInt(schedule[releases - 1].amount)).toEqual(
         (100n % 7n) + 100n / 7n
     );
 });
 
-test('createSchedule should split amount equally, if the releases divide the amount', () => {
+test('createRegularIntervalSchedule should split amount equally, if the releases divide the amount', () => {
     const totalAmount = 100n;
     const releases = 5;
-    const schedule = createSchedule(totalAmount, releases, 0, 0);
+    const schedule = createRegularIntervalSchedule(totalAmount, releases, 0, 0);
     schedule.forEach((point) => expect(point.amount).toEqual('20'));
 });
 
-test('createSchedule should increase timestamps by interval', () => {
+test('createRegularIntervalSchedule should increase timestamps by interval', () => {
     const interval = 3;
     const start = 5;
     const releases = 7;
-    const schedule = createSchedule(0n, releases, start, interval);
+    const schedule = createRegularIntervalSchedule(
+        0n,
+        releases,
+        start,
+        interval
+    );
     expect(
         schedule.reduce(
             (acc, { timestamp }, index) =>
