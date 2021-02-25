b3c86098c58f746ce759cf90e81582e19cde9c50
Søren Bruus Zeppelin <sz@concordium.com>
2021-02-25 10:57:14 +0100

    Added classname prop

--- app/components/PageContainer/PageContainer.stories.tsx
+++ app/components/PageContainer/PageContainer.stories.tsx
@@ -2,6 +2,7 @@ import React, { PropsWithChildren } from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
 
+import clsx from 'clsx';
 import PageContainer, { PageContainerProps } from './PageContainer';
 
 export default {
@@ -12,21 +13,24 @@ export default {
             <>
                 <style>
                     {`
-                        .sb-hack-wrapper > * {
+                        .sb-page-container {
                             height: 800px;
                             min-width: 900px;
                             border: 1px solid grey;
                         }
                     `}
                 </style>
-                <span className="sb-hack-wrapper">{story()}</span>
+                {story()}
             </>
         ),
     ],
 } as Meta;
 
 const Template: Story<PropsWithChildren<PageContainerProps>> = (args) => (
-    <PageContainer {...args} />
+    <PageContainer
+        {...args}
+        className={clsx('sb-page-container', args.className)}
+    />
 );
 
 export const Primary = Template.bind({});
--- app/components/PageContainer/PageContainer.tsx
+++ app/components/PageContainer/PageContainer.tsx
@@ -5,14 +5,18 @@ import styles from './PageContainer.module.scss';
 
 export interface PageContainerProps {
     boxed?: boolean;
+    className?: string;
 }
 
 export default function PageContainer({
     children,
     boxed = false,
+    className,
 }: PropsWithChildren<PageContainerProps>): JSX.Element {
     return (
-        <section className={clsx(styles.root, boxed && styles.rootBoxed)}>
+        <section
+            className={clsx(styles.root, boxed && styles.rootBoxed, className)}
+        >
             {boxed ? <div className={styles.box}>{children}</div> : children}
         </section>
     );
