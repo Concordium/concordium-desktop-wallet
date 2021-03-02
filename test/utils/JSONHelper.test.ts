658e6c6e2c75db9598a9fd42196350dfbe264c11
Hjort <shj@concordium.com>
2021-03-02 13:25:55 +0100

    Added more tests to stringify/parse

--- test/utils/JSONHelper.test.ts
+++ test/utils/JSONHelper.test.ts
@@ -9,3 +9,30 @@ test('parse/stringify handles a bigint', () => {
     const x = BigInt(5);
     expect(typeof parse(stringify(x))).toBe('bigint');
 });
+
+test('parse/stringify handles a bigint and preserves the value', () => {
+    const x = BigInt(5);
+    expect(parse(stringify(x))).toBe(5n);
+});
+
+test('parse/stringify should not convert strings, that corresponds bigint literals, to bigints.', () => {
+    const x = '5n';
+    expect(parse(stringify(x))).toBe('5n');
+});
+
+test('parse/stringify should handle a list of bigints and preserves the values', () => {
+    const x = [1n, 2n, 5n];
+    const y = parse(stringify(x));
+    expect(y).toHaveLength(3);
+    expect(y[0]).toBe(1n);
+    expect(y[1]).toBe(2n);
+    expect(y[2]).toBe(5n);
+});
+
+test('parse/stringify should handle an object with a bigint field and preserves the value', () => {
+    const x = { big: 2n, small: 2, text: '2n' };
+    const y = parse(stringify(x));
+    expect(y.big).toBe(2n);
+    expect(y.small).toBe(2);
+    expect(y.text).toBe('2n');
+});
