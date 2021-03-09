6b28f2839ed4d252f8ae23d7dba4c0fa97a1a0d6
Jakob Ørhøj <jo@concordium.com>
2021-03-09 11:21:20 +0100

    Remove unused settings and refactor

--- app/database/migrations/20210104134509_init_setting_group.ts
+++ app/database/migrations/20210104134509_init_setting_group.ts
@@ -1,9 +1,13 @@
 import * as Knex from 'knex';
+import settingKeys from '../../constants/settingKeys.json';
 
 export async function up(knex: Knex): Promise<void> {
     return knex
         .table('setting_group')
-        .insert([{ name: 'multisig' }, { name: 'node' }, { name: 'other' }]);
+        .insert([
+            { name: settingKeys.multiSignatureSettings },
+            { name: settingKeys.nodeSettings },
+        ]);
 }
 
 export async function down(knex: Knex): Promise<void> {
