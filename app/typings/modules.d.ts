5865a1318d16d7b1938654ae108906e76e0a647d
Søren Bruus Zeppelin <sz@concordium.com>
2021-03-16 15:21:02 +0100

    Added bg image

--- app/typings/modules.d.ts
+++ app/typings/modules.d.ts
@@ -3,5 +3,10 @@ declare module '*.svg' {
     const content: any;
     export default content;
 }
+declare module '*.png' {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const content: any;
+    export default content;
+}
 
 declare module 'react-identicons';
