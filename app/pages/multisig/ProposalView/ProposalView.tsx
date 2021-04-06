559595cd6d8b7dbafa184035b344e37f553b4ca0
Søren Bruus Zeppelin <sz@concordium.com>
2021-04-06 10:45:16 +0200

    disable submit on non-open proposals

--- app/pages/multisig/ProposalView/ProposalView.tsx
+++ app/pages/multisig/ProposalView/ProposalView.tsx
@@ -379,13 +379,12 @@ function ProposalView({ proposal }: ProposalViewProps) {
                                     className={styles.finalCheckbox}
                                     name="finalCheck"
                                     rules={{ required: true }}
-                                    // disabled={!isOpen}
+                                    disabled={!isOpen}
                                 >
                                     I understand this is the final submission
                                     and cannot be reverted
                                 </Form.Checkbox>
-                                {/* <Form.Submit disabled={!isOpen}> */}
-                                <Form.Submit>
+                                <Form.Submit disabled={!isOpen}>
                                     Submit transaction to chain
                                 </Form.Submit>
                             </div>
