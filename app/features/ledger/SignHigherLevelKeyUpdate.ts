c3ff5b152017a0bd367328b0d4017cfc236ff6d0
Jakob Ørhøj <jo@concordium.com>
2021-04-08 14:44:08 +0200

    Init serialization for ledger of higher level keys

---
+++ app/features/ledger/SignHigherLevelKeyUpdate.ts
@@ -0,0 +1,68 @@
+/* eslint-disable no-await-in-loop */
+import type Transport from '@ledgerhq/hw-transport';
+import { HigherLevelKeyUpdate, UpdateInstruction } from '../../utils/types';
+import pathAsBuffer from './Path';
+import {
+    serializeUpdateHeader,
+    serializeUpdateType,
+} from '../../utils/UpdateSerialization';
+import { serializeVerifyKey } from '~/utils/serializationHelpers';
+
+// TODO It should take this as input instead, so we can support all 3 types.
+const INS = 0x28;
+
+export default async function signHigherLevelKeyUpdate(
+    transport: Transport,
+    path: number[],
+    transaction: UpdateInstruction<HigherLevelKeyUpdate>,
+    serializedPayload: Buffer
+): Promise<Buffer> {
+    const updateHeaderWithPayloadSize = {
+        ...transaction.header,
+        payloadSize: serializedPayload.length + 1,
+    };
+
+    const serializedHeader = serializeUpdateHeader(updateHeaderWithPayloadSize);
+    const serializedUpdateType = serializeUpdateType(transaction.type);
+
+    let p1 = 0x00;
+    const p2 = 0x00;
+
+    const serializedKeyUpdateType = Buffer.alloc(1);
+    serializedKeyUpdateType.writeInt8(transaction.payload.keyUpdateType, 0);
+
+    const updateKeysLength = transaction.payload.updateKeys.length;
+    const serializedNumberOfUpdateKeys = Buffer.alloc(2);
+    serializedNumberOfUpdateKeys.writeUInt16BE(updateKeysLength, 0);
+
+    const initialData = Buffer.concat([
+        pathAsBuffer(path),
+        serializedHeader,
+        serializedUpdateType,
+        serializedKeyUpdateType,
+        serializedNumberOfUpdateKeys,
+    ]);
+    await transport.send(0xe0, INS, p1, p2, initialData);
+
+    p1 = 0x01;
+    for (let i = 0; i < updateKeysLength; i += 1) {
+        const index = i;
+        const verificationKey = transaction.payload.updateKeys[index];
+
+        const data = Buffer.concat([
+            Uint8Array.of(index),
+            serializeVerifyKey(verificationKey),
+        ]);
+
+        // eslint-disable-next-line  no-await-in-loop
+        await transport.send(0xe0, INS, p1, p2, data);
+    }
+
+    p1 = 0x02;
+    const thresholdData = Buffer.alloc(2);
+    thresholdData.writeUInt16BE(transaction.payload.threshold, 0);
+
+    const response = await transport.send(0xe0, INS, p1, p2, thresholdData);
+    const signature = response.slice(0, 64);
+    return signature;
+}
