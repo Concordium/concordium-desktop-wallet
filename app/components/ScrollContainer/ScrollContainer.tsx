bbba7c8d15eb1a07802747fd77f22442ca3692f8
Søren Bruus Zeppelin <sz@concordium.com>
2021-04-12 13:52:25 +0200

    scrollbar styling

--- app/components/ScrollContainer/ScrollContainer.tsx
+++ app/components/ScrollContainer/ScrollContainer.tsx
@@ -1,19 +1,31 @@
+import clsx from 'clsx';
 import React, { PropsWithChildren } from 'react';
 
 import styles from './ScrollContainer.module.scss';
 
-type ScrollContainerProps = PropsWithChildren<unknown>;
+type ScrollContainerProps = PropsWithChildren<{
+    /**
+     * Defaults to y.
+     */
+    direction?: 'y' | 'x' | 'both';
+}>;
 
 /**
  * @description
  * Exists to inset vertical scroll bar.
  */
 export default function ScrollContainer({
+    direction = 'y',
     children,
 }: ScrollContainerProps): JSX.Element {
     return (
-        <div className={styles.root}>
-            <div className={styles.content}>{children}</div>
+        <div
+            className={clsx(
+                (direction === 'x' || direction === 'both') && styles.x,
+                (direction === 'y' || direction === 'both') && styles.y
+            )}
+        >
+            {children}
         </div>
     );
 }
