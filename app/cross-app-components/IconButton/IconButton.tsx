89acb1c8c1c169facc9d49184464dbffd4848a00
Søren Bruus Zeppelin <sz@concordium.com>
2021-04-12 11:34:49 +0200

    streamline icon buttons

---
+++ app/cross-app-components/IconButton/IconButton.tsx
@@ -0,0 +1,24 @@
+import clsx from 'clsx';
+import React, { ButtonHTMLAttributes, PropsWithChildren } from 'react';
+
+import Button from '../Button';
+import styles from './IconButton.module.scss';
+
+export type IconButtonProps = PropsWithChildren<
+    ButtonHTMLAttributes<HTMLButtonElement>
+>;
+
+export default function CloseButton({
+    className,
+    ...props
+}: IconButtonProps): JSX.Element {
+    return (
+        <Button
+            type="button"
+            title="close"
+            clear
+            className={clsx(styles.root, className)}
+            {...props}
+        />
+    );
+}
