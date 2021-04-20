a872fddd2dc77efd124f0d4fd992a81739c59ef8
Jens S <jas@concordium.com>
2021-04-20 14:27:28 +0200

    Adjusted and cleaned up notarize script a bit

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -1,18 +1,28 @@
-require('dotenv').config();
+/* eslint-disable @typescript-eslint/no-var-requires */
 const { notarize } = require('electron-notarize');
 
 exports.default = async function notarizing(context) {
-  const { electronPlatformName, appOutDir } = context;  
-  if (electronPlatformName !== 'darwin') {
-    return;
-  }
+    const { electronPlatformName, appOutDir } = context;
+    if (electronPlatformName !== 'darwin') {
+        return 'Skipping MacOS signing and notarizing';
+    }
 
-  const appName = context.packager.appInfo.productFilename;
+    if (!process.env.APPLEID || !process.env.APPLEIDPASS) {
+        throw new Error(
+            'The APPLEID or APPLEIDPASS environment variables used for notarizing the application was not set correctly.'
+        );
+    }
 
-  return await notarize({
-    appBundleId: 'org.Concordium.DesktopWallet',
-    appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLEID,
-    appleIdPassword: process.env.APPLEIDPASS,
-  });
+    const appName = context.packager.appInfo.productFilename;
+
+    try {
+        return notarize({
+            appBundleId: 'org.Concordium.DesktopWallet',
+            appPath: `${appOutDir}/${appName}.app`,
+            appleId: process.env.APPLEID,
+            appleIdPassword: process.env.APPLEIDPASS,
+        });
+    } catch (e) {
+        return e;
+    }
 };
