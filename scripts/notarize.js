2cdfd3ef663365e82686d70243f49a932397cb3c
Jens S <jas@concordium.com>
2021-04-21 10:02:28 +0200

    Changed the skip notarize script, as it was not building

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -3,12 +3,9 @@ const { notarize } = require('electron-notarize');
 
 exports.default = async function notarizing(context) {
     const { electronPlatformName, appOutDir } = context;
-    if (
-        electronPlatformName !== 'darwin' ||
-        process.env.BUILDMODE === 'development'
-    ) {
+    if (electronPlatformName !== 'darwin' || process.env.NOTARIZE === 'skip') {
         return console.log(
-            'Not building for production, skipping notarization process.'
+            'Not building for MacOS or for production, skipping notarization process.'
         );
     }
 
