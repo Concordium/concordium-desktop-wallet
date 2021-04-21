adbe9dcabbb0fcabe8b83c74b7d6a60e259de5ee
Hjort <shj@concordium.com>
2021-04-21 11:18:38 +0200

    fix tests and lints errors

--- app/pages/multisig/AccountTransactions/CreateUpdate.tsx
+++ app/pages/multisig/AccountTransactions/CreateUpdate.tsx
@@ -56,6 +56,7 @@ export default function CreateUpdate({
         account,
         addedCredentials,
         removedCredIds,
+        currentCredentialAmount,
         newThreshold,
     ]);
 
--- app/utils/transactionHelpers.ts
+++ app/utils/transactionHelpers.ts
@@ -25,7 +25,7 @@ import {
     getTransactionKindEnergy,
     getUpdateAccountCredentialEnergy,
 } from './transactionCosts';
-import { toMicroUnits, isValidGTUString } from '~/utils/gtu';
+import { toMicroUnits, isValidGTUString } from './gtu';
 
 /**
  * Attempts to find the address in the accounts, and then AddressBookEntries
