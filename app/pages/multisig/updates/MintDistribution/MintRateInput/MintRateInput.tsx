ffe3673f077529861ba4f5b5931642ee9e8da6b3
Søren Bruus Zeppelin <sz@concordium.com>
2021-04-21 08:37:34 +0200

    with validation

--- app/pages/multisig/updates/MintDistribution/MintRateInput/MintRateInput.tsx
+++ app/pages/multisig/updates/MintDistribution/MintRateInput/MintRateInput.tsx
@@ -1,7 +1,14 @@
 import clsx from 'clsx';
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import { FormProvider, useForm } from 'react-hook-form';
+import {
+    FormProvider,
+    RegisterOptions,
+    useForm,
+    useFormContext,
+    Validate,
+} from 'react-hook-form';
 import Form from '~/components/Form';
+import ErrorMessage from '~/components/Form/ErrorMessage';
 import { useUpdateEffect } from '~/utils/hooks';
 import { ClassName, EqualRecord } from '~/utils/types';
 
@@ -29,6 +36,23 @@ const innerFieldNames: EqualRecord<InnerFields> = {
     mintPerSlot: 'mintPerSlot',
 };
 
+const innerFieldValidation: Partial<RegisterOptions> = {
+    required: 'Field is required',
+    min: {
+        value: 0,
+        message: "Value can't be negative",
+    },
+    validate: {
+        validNumber: (v: string) => {
+            const p = parseFloat(v);
+
+            return (
+                (!Number.isNaN(p) && p !== 0) || 'Invalid mint per slot value'
+            );
+        },
+    },
+};
+
 export interface MintRateInputProps extends ClassName {
     mintPerSlot: number;
     slotsPerYear: number;
@@ -41,9 +65,15 @@ export default function MintRateInput({
     disabled = false,
     className,
 }: MintRateInputProps): JSX.Element {
-    const innerForm = useForm<InnerFields>();
-    const { watch, setValue } = innerForm;
+    const innerForm = useForm<InnerFields>({ mode: 'onChange' });
+    const { watch, setValue, errors: innerErrors, formState } = innerForm;
+    const isTouched = Boolean(Object.keys(formState.touched).length);
+
+    const { errors } = useFormContext<MintRateInputFields>();
     const [anualFocused, setAnualFocused] = useState<boolean>(false);
+    const errorMessage =
+        Object.values(innerErrors)[0]?.message ??
+        Object.values(errors)[0]?.message;
 
     const calculateAnualRate = useCallback(
         (m: number) => (1 + Number(m)) ** Number(slotsPerYear) - 1,
@@ -55,7 +85,7 @@ export default function MintRateInput({
     );
 
     const initialAnualRate = calculateAnualRate(mintPerSlot).toString();
-    const fields = watch(
+    const innerFields = watch(
         [innerFieldNames.mintPerSlot, innerFieldNames.anualRate],
         {
             [innerFieldNames.mintPerSlot]: mintRateFormat(mintPerSlot),
@@ -64,23 +94,30 @@ export default function MintRateInput({
     );
 
     useEffect(() => {
-        const calculated = calculateAnualRate(Number(fields.mintPerSlot));
+        const calculated = calculateAnualRate(Number(innerFields.mintPerSlot));
         setValue(innerFieldNames.anualRate, calculated.toString());
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [fields.mintPerSlot]);
+    }, [innerFields.mintPerSlot]);
 
     useUpdateEffect(() => {
         if (!anualFocused) {
             return;
         }
 
-        const calculated = calculateMintPerSlot(Number(fields.anualRate));
+        const calculated = calculateMintPerSlot(Number(innerFields.anualRate));
         setValue(innerFieldNames.mintPerSlot, mintRateFormat(calculated));
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [fields.anualRate]);
+    }, [innerFields.anualRate]);
+
+    const innerValid: Validate = useCallback(() => {
+        if (!Object.keys(innerErrors).length) {
+            return true;
+        }
+        return Object.values(innerErrors)[0]?.message;
+    }, [innerErrors]);
 
     const { mantissa, exponent } = useMemo(() => {
-        const [m, e = '0'] = fields.mintPerSlot.toLowerCase().split('e-');
+        const [m, e = '0'] = innerFields.mintPerSlot.toLowerCase().split('e-');
         const [, fractions = ''] = m.split('.');
 
         if (!fractions) {
@@ -94,13 +131,14 @@ export default function MintRateInput({
             mantissa: BigInt(m.replace('.', '')).toString(),
             exponent: parseInt(e, 10) + fractions.length,
         };
-    }, [fields.mintPerSlot]);
+    }, [innerFields.mintPerSlot]);
 
     return (
         <span className={clsx(styles.root, className)}>
             <div>
                 <FormProvider {...innerForm}>
                     <span>
+                        {/* {JSON.stringify(errors)} {JSON.stringify(innerErrors)} */}
                         <Form.InlineNumber
                             className={styles.field}
                             name={innerFieldNames.anualRate}
@@ -109,13 +147,7 @@ export default function MintRateInput({
                             defaultValue={initialAnualRate}
                             onFocus={() => setAnualFocused(true)}
                             onBlur={() => setAnualFocused(false)}
-                            rules={{
-                                required: 'Field is required',
-                                min: {
-                                    value: 0,
-                                    message: "Value can't be negative",
-                                },
-                            }}
+                            rules={innerFieldValidation}
                             disabled={disabled}
                         />{' '}
                         ≈ (1 +{' '}
@@ -124,13 +156,7 @@ export default function MintRateInput({
                             name={innerFieldNames.mintPerSlot}
                             defaultValue={mintRateFormat(mintPerSlot)}
                             title="Mint per slot (product of chain value)"
-                            rules={{
-                                required: 'Field is required',
-                                min: {
-                                    value: 0,
-                                    message: "Value can't be negative",
-                                },
-                            }}
+                            rules={innerFieldValidation}
                             disabled={disabled}
                             customFormatter={(v = '') => {
                                 if (v === '') {
@@ -155,6 +181,7 @@ export default function MintRateInput({
                     Chain value: {mantissa}E-{exponent} (Mantissa: {mantissa},
                     Exponent: {exponent})
                 </div>
+                <ErrorMessage>{isTouched && errorMessage}</ErrorMessage>
             </div>
             {!disabled && (
                 <>
@@ -163,12 +190,21 @@ export default function MintRateInput({
                         type="hidden"
                         value={mantissa}
                         readOnly
+                        rules={{
+                            required:
+                                'Mint per slot produces invalid mantissa value',
+                            validate: innerValid,
+                        }}
                     />
                     <Form.Input
                         name={fieldNames.exponent}
                         type="hidden"
                         value={exponent}
                         readOnly
+                        rules={{
+                            required:
+                                'Mint per slot produces invalid exponent value',
+                        }}
                     />
                 </>
             )}
