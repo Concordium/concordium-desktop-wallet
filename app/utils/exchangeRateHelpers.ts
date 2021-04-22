a17d9bb06826c939e09bb41a8b383e21a3317b02
Søren Bruus Zeppelin <sz@concordium.com>
2021-04-22 14:35:43 +0200

    jsdoc

--- app/utils/exchangeRateHelpers.ts
+++ app/utils/exchangeRateHelpers.ts
@@ -1,5 +1,14 @@
 const getGCD = (a: bigint, b: bigint): bigint => (b ? getGCD(b, a % b) : a);
 
+/**
+ * @description
+ * Reduces fraction of 2 bigints
+ *
+ * @returns tuple of reduced numerator & denominator.
+ *
+ * @example
+ * reduceExchangeRate(2n, 4n) => [1n, 2n]
+ */
 // eslint-disable-next-line import/prefer-default-export
 export function reduceExchangeRate(
     numerator: bigint,
