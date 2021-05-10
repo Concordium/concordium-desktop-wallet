f66d5cf91f401d1904178d272a176d4412f58de6
Søren Bruus Zeppelin <sz@concordium.com>
2021-05-10 09:34:39 +0200

    moved pickaccount

--- app/pages/GenerateCredential/PickAccount/PickAccount.tsx
+++ app/pages/GenerateCredential/PickAccount/PickAccount.tsx
@@ -7,13 +7,13 @@ import AccountListElement from '~/components/AccountListElement';
 import { isValidAddress } from '~/utils/accountHelpers';
 import { getAccountInfoOfAddress } from '~/utils/nodeHelpers';
 import Button from '~/cross-app-components/Button';
-import RevealAttributes from './RevealAttributes';
+import RevealAttributes from '../RevealAttributes';
 import routes from '~/constants/routes.json';
-import styles from './GenerateCredential.module.scss';
+import styles from './PickAccount.module.scss';
 import ConnectionStatusComponent, {
     Status,
 } from '~/components/ConnectionStatusComponent';
-import generateCredentialContext from './GenerateCredentialContext';
+import generateCredentialContext from '../GenerateCredentialContext';
 
 const addressLength = 50;
 const mustBeDeployedMessage = 'Address must belong to an deployed account';
@@ -115,16 +115,24 @@ export default function PickAccount({
         );
     }
 
+    const isRevealAttributesRoute =
+        location === routes.GENERATE_CREDENTIAL_REVEALATTRIBUTES;
+
     return (
         <>
-            {accountDisplay}
-            {location === routes.GENERATE_CREDENTIAL_REVEALATTRIBUTES ? (
-                <RevealAttributes
-                    setChosenAttributes={setChosenAttributes}
-                    identity={identity}
-                />
-            ) : (
+            <div className={styles.accountWrapper}>
+                {accountDisplay}
+                {isRevealAttributesRoute && (
+                    <RevealAttributes
+                        setChosenAttributes={setChosenAttributes}
+                        identity={identity}
+                    />
+                )}
+            </div>
+            {!isRevealAttributesRoute && (
                 <Button
+                    className={styles.button}
+                    inverted
                     disabled={!isReady}
                     onClick={() =>
                         dispatch(
