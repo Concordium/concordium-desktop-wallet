b4bed46e2831587b261cec70ff559e213c8be8f6
Hjort <shj@concordium.com>
2021-05-18 17:23:03 +0200

    use script for packaging to have nicer names

---
+++ scripts/build.js
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// Check if the renderer and main bundles are built
+const path = require('path');
+const chalk = require('chalk');
+const fs = require('fs');
+
+const builder = require('electron-builder');
+
+const publish = process.env.NODE_ENV;
+
+const targetNet = process.env.TARGET_NET;
+
+let name;
+let productName;
+if (targetNet) {
+    name = `${process.env.npm_package_name}-${targetNet}`;
+    productName = `${process.env.npm_package_productName} ${targetNet}`;
+} else {
+    name = process.env.npm_package_name;
+    productName = process.env.npm_package_productName;
+}
+
+builder.build({
+    config: {
+        extraMetadata: {
+            name,
+            productName,
+        },
+    },
+    publish: 'never',
+});
