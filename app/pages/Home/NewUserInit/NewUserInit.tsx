55aabc4dcd1e17961ef2791ec53bdf599a6aa1f5
Søren Bruus Zeppelin <sz@concordium.com>
2021-05-19 14:48:24 +0200

    new user page

---
+++ app/pages/Home/NewUserInit/NewUserInit.tsx
@@ -0,0 +1,38 @@
+import { push } from 'connected-react-router';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import PageLayout from '~/components/PageLayout/PageLayout';
+import Button from '~/cross-app-components/Button/Button';
+import routes from '~/constants/routes.json';
+import styles from './NewUserInit.module.scss';
+
+export default function NewUserInit() {
+    const dispatch = useDispatch();
+
+    return (
+        <PageLayout.Container disableBack>
+            <div className={styles.content}>
+                <div className="flexChildFill flexColumn justifyCenter">
+                    <h2 className="mB50">Hi, there!</h2>
+                    <p>
+                        Before you can start using the Concordium Desktop
+                        Wallet, you have to set up a few security measures.
+                        Press Continue to be guided through the setup process.
+                    </p>
+                </div>
+                <Button
+                    className={styles.button}
+                    onClick={() =>
+                        dispatch(
+                            push({
+                                pathname: routes.HOME_SELECT_PASSWORD,
+                            })
+                        )
+                    }
+                >
+                    Continue
+                </Button>
+            </div>
+        </PageLayout.Container>
+    );
+}
