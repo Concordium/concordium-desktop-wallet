5e8b5b8c286a5b579451a393835ef14f75a027fe
Hjort <shj@concordium.com>
2021-05-21 10:19:04 +0200

    removed unused rewardFilter enum

--- app/utils/types.ts
+++ app/utils/types.ts
@@ -1136,12 +1136,6 @@ export type PolymorphicComponentProps<
     Props = {}
 > = InheritableElementProps<C, Props & AsProp<C>>;
 
-export enum RewardFilter {
-    None,
-    AllButFinalization,
-    All,
-}
-
 export enum TransactionTypes {
     UpdateInstruction,
     AccountTransaction,
