84442a798cb89e61bd701abc1fad621b6559530f
Jakob Ørhøj <jo@concordium.com>
2021-05-27 15:48:12 +0200

    Update block hash on finalization

--- app/features/TransactionSlice.ts
+++ app/features/TransactionSlice.ts
@@ -258,41 +258,32 @@ export async function addPendingTransaction(
     return insertTransactions([convertedTransaction]);
 }
 
-// Set the transaction's status to confirmed, update the cost and whether it succeded.
-// TODO: update Total to reflect change in cost.
+/**
+ * Set the transaction's status to confirmed, update the cost and whether it suceeded
+ * or not.
+ */
 export async function confirmTransaction(
     dispatch: Dispatch,
     transactionHash: string,
-    outcomeRecord: Record<string, TransactionEvent>
+    blockHash: string,
+    event: TransactionEvent
 ) {
-    const outcomes = Object.values(outcomeRecord);
-    const success = isSuccessfulTransaction(outcomes);
-    const cost = outcomes.reduce(
-        (accu, event) => accu + parseInt(event.cost, 10),
-        0
-    );
+    const success = isSuccessfulTransaction(event);
+    const { cost } = event;
+
     let rejectReason;
     if (!success) {
-        const failure = outcomes.find(
-            (event) => event.result.outcome !== 'success'
-        );
-        if (!failure) {
-            throw new Error('Missing failure for unsuccessful transaction');
-        }
-        if (!failure.result) {
-            throw new Error('Missing failure result');
-        }
-        if (!failure.result.rejectReason) {
-            throw new Error('Missing rejection reason in failure result');
+        if (!event.result.rejectReason) {
+            throw new Error('Missing rejection reason in transaction event');
         }
 
         rejectReason =
             RejectReason[
-                failure.result.rejectReason.tag as keyof typeof RejectReason
+                event.result.rejectReason.tag as keyof typeof RejectReason
             ];
         if (rejectReason === undefined) {
             // If the reject reason was not known, then just store it directly as a string anyway.
-            rejectReason = failure.result.rejectReason.tag;
+            rejectReason = event.result.rejectReason.tag;
         }
     }
 
@@ -301,6 +292,7 @@ export async function confirmTransaction(
         cost: cost.toString(),
         success,
         rejectReason,
+        blockHash,
     };
     updateTransaction({ transactionHash }, update);
     return dispatch(
