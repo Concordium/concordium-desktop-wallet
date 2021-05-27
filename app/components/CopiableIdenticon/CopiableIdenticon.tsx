cd4a68fbfc0255d0049763c6c6c6dc56e52a96c8
Søren Bruus Zeppelin <sz@concordium.com>
2021-05-27 10:01:12 +0200

    added copy indication to identicon

--- app/components/CopiableIdenticon/CopiableIdenticon.tsx
+++ app/components/CopiableIdenticon/CopiableIdenticon.tsx
@@ -4,27 +4,31 @@ import Identicon from 'react-identicons';
 // @ts-ignore
 import { useScreenshot } from 'use-react-screenshot';
 import { clipboard, nativeImage } from 'electron';
+import clsx from 'clsx';
+
+import CheckmarkIcon from '@resources/svg/checkmark-blue.svg';
+import { useTimeoutState } from '~/utils/hooks';
+
 import styles from './CopiableIdenticon.module.scss';
 
 const imageWidth = 128;
 
-interface Props {
+export interface CopiableIdenticonProps {
     data: string;
     setScreenshot?: (dataUrl: string) => void;
-}
-
-function copyIdenticonToClipboard(dataUrl: string) {
-    const image = nativeImage.createFromDataURL(dataUrl);
-    return clipboard.writeImage(image);
 }
 
 /**
  * Component that displays a copiable identicon. Clicking the identicon will
  * copy the identicon to the clipboard as a PNG file.
  */
-export default function CopiableIdenticon({ data, setScreenshot }: Props) {
+export default function CopiableIdenticon({
+    data,
+    setScreenshot,
+}: CopiableIdenticonProps) {
     const ref = useRef(null);
     const [image, takeScreenshot] = useScreenshot();
+    const [showCopied, setShowCopied] = useTimeoutState<boolean>(false, 2000);
 
     // Take a screenshot of the identicon after the component has rendered
     // the first time.
@@ -35,6 +39,12 @@ export default function CopiableIdenticon({ data, setScreenshot }: Props) {
             setScreenshot(image);
         }
     }, [image, takeScreenshot, setScreenshot]);
+
+    function copyIdenticonToClipboard(dataUrl: string) {
+        setShowCopied(true);
+        const img = nativeImage.createFromDataURL(dataUrl);
+        return clipboard.writeImage(img);
+    }
 
     return (
         <>
@@ -47,7 +57,17 @@ export default function CopiableIdenticon({ data, setScreenshot }: Props) {
                 className={styles.identicon}
                 onClick={() => copyIdenticonToClipboard(image)}
                 type="button"
+                disabled={showCopied}
             >
+                <div
+                    className={clsx(
+                        styles.copied,
+                        showCopied && styles.copiedVisible
+                    )}
+                >
+                    <CheckmarkIcon />
+                    Copied!
+                </div>
                 <Identicon string={data} size={imageWidth} />
             </button>
         </>
