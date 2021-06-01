ac7885acd881e7bd9ad65ed8ba4e0e85028a4ebc
Hjort <shj@concordium.com>
2021-06-01 15:26:59 +0200

    simplify PublicKeyDetails

--- app/components/ledger/PublicKeyDetails.tsx
+++ app/components/ledger/PublicKeyDetails.tsx
@@ -6,20 +6,14 @@ interface Props {
     publickey: string;
 }
 
-const firstLineLength = 18;
 const lineLength = 18;
 
 export default function PublicKeyDetails({ publickey }: Props) {
-    const firstLine = publickey.substring(0, firstLineLength);
-
     return (
         <div className="textCenter mV40">
-            <p>{firstLine}</p>
-            {chunkString(publickey.substring(firstLineLength), lineLength).map(
-                (text) => (
-                    <p key={text}>{text}</p>
-                )
-            )}
+            {chunkString(publickey, lineLength).map((text) => (
+                <p key={text}>{text}</p>
+            ))}
         </div>
     );
 }
