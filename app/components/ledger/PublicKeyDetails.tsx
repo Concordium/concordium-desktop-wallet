7a47b3ed05d3c7a39dc311b865e1ba1d011d9a2a
Hjort <shj@concordium.com>
2021-06-02 09:27:57 +0200

    Removed margin from PublikeyDetails lines

--- app/components/ledger/PublicKeyDetails.tsx
+++ app/components/ledger/PublicKeyDetails.tsx
@@ -12,7 +12,9 @@ export default function PublicKeyDetails({ publickey }: Props) {
     return (
         <div className="textCenter mV40">
             {chunkString(publickey, lineLength).map((text) => (
-                <p key={text}>{text}</p>
+                <p className="m0" key={text}>
+                    {text}
+                </p>
             ))}
         </div>
     );
