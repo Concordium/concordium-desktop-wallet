3de1aaf2892d93ca6449a14c66cc7705d555588a
Hjort <shj@concordium.com>
2021-06-04 17:00:45 +0200

    Dont reset transactionTable

--- app/database/migrations/20210604084100_reset_transaction_and_multisig.ts
+++ app/database/migrations/20210604084100_reset_transaction_and_multisig.ts
@@ -1,13 +1,11 @@
 /* eslint-disable */
 import { Knex } from 'knex';
 import {
-    transactionTable,
     multiSignatureProposalTable,
 } from '~/constants/databaseNames.json';
 
 export async function up(knex: Knex): Promise<void> {
-    await knex.table(transactionTable).del();
-    await knex.table(multiSignatureProposalTable).del();
+    return knex.table(multiSignatureProposalTable).del();
 }
 
 export async function down(_knex: Knex): Promise<void> {}
