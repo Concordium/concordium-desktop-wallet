1c58355cbe37c38311e9a39d3d3f4670508522d6
Jakob Ørhøj <jo@concordium.com>
2021-06-04 08:51:30 +0200

    Move license notices URL to constants file

--- app/pages/settings/About/LicenseNotices/LicenseNotices.tsx
+++ app/pages/settings/About/LicenseNotices/LicenseNotices.tsx
@@ -4,13 +4,11 @@ import React, { useEffect } from 'react';
 import { Redirect } from 'react-router';
 import ipcCommands from '../../../../constants/ipcCommands.json';
 import routes from '~/constants/routes.json';
-
-// TODO Update the link to the mainnet URL.
-const licenseNoticesUrl = 'https://concordium.com/';
+import { licenseNotices } from '~/constants/urls.json';
 
 export default function LicenseNotices() {
     useEffect(() => {
-        ipcRenderer.invoke(ipcCommands.openUrl, licenseNoticesUrl);
+        ipcRenderer.invoke(ipcCommands.openUrl, licenseNotices);
     }, []);
 
     return <Redirect to={routes.SETTINGS_ABOUT} />;
