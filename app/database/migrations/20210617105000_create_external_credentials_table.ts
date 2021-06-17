5a4b196371ccfdf54c2f72c581a3bd9b3e68197b
Søren Bruus Zeppelin <sz@concordium.com>
2021-06-17 11:31:13 +0200

    Add external credentials table

---
+++ app/database/migrations/20210617105000_create_external_credentials_table.ts
@@ -0,0 +1,21 @@
+import { Knex } from 'knex';
+import {
+    externalCredentialsTable,
+    accountsTable,
+} from '~/constants/databaseNames.json';
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable(externalCredentialsTable, (table) => {
+        table
+            .string('address')
+            .references('address')
+            .inTable(accountsTable)
+            .notNullable();
+        table.string('credId').primary();
+        table.string('note').defaultTo('');
+    });
+}
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTable(externalCredentialsTable);
+}
