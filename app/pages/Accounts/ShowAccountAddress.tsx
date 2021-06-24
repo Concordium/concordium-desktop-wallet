8d92ac27cd6ff5cdc28a27bc96722d549a3749ba
Hjort <shj@concordium.com>
2021-06-24 14:01:20 +0200

    Added fullscreen account address view

--- app/pages/Accounts/ShowAccountAddress.tsx
+++ app/pages/Accounts/ShowAccountAddress.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import QRCode from 'qrcode.react';
+import { push } from 'connected-react-router';
+import ExpandIcon from '@resources/svg/expand.svg';
 import { Account } from '../../utils/types';
 import CopyButton from '../../components/CopyButton';
 import CloseButton from '~/cross-app-components/CloseButton';
 import Card from '~/cross-app-components/Card';
 import styles from './Accounts.module.scss';
+import routes from '~/constants/routes.json';
+import IconButton from '~/cross-app-components/IconButton';
 
 interface Props {
     account: Account;
@@ -15,8 +20,16 @@ interface Props {
  * Displays the account's address, and allows the user to copy it.
  */
 export default function ShowAccountAddress({ account, returnFunction }: Props) {
+    const dispatch = useDispatch();
+
     return (
         <Card className="flexColumn alignCenter relative pH50">
+            <IconButton
+                className={styles.expandButton}
+                onClick={() => dispatch(push(routes.SHOWADDRESS))}
+            >
+                <ExpandIcon height="30" />
+            </IconButton>
             <CloseButton
                 className={styles.closeButton}
                 onClick={returnFunction}
---
+++ app/pages/Accounts/ShowAccountAddressFull.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import QRCode from 'qrcode.react';
+import ShrinkIcon from '@resources/svg/shrink.svg';
+import { push } from 'connected-react-router';
+import { chosenAccountSelector } from '~/features/AccountSlice';
+import CopyButton from '../../components/CopyButton';
+import PageLayout from '~/components/PageLayout';
+import routes from '~/constants/routes.json';
+import styles from './Accounts.module.scss';
+import IconButton from '~/cross-app-components/IconButton';
+
+/**
+ * Displays the account's address, and allows the user to copy it.
+ */
+export default function ShowAccountAddress() {
+    const dispatch = useDispatch();
+    const account = useSelector(chosenAccountSelector);
+
+    if (!account) {
+        return null;
+    }
+
+    return (
+        <PageLayout>
+            <PageLayout.Header>
+                <h1>Accounts</h1>
+            </PageLayout.Header>
+            <PageLayout.Container
+                closeRoute={routes.ACCOUNTS}
+                disableBack
+                padding="vertical"
+                className={styles.showAddressContainer}
+            >
+                <IconButton
+                    className={styles.expandButton}
+                    onClick={() => dispatch(push(routes.ACCOUNTS_MORE_ADDRESS))}
+                >
+                    <ShrinkIcon width="30" />
+                </IconButton>
+
+                <h2 className="m0 mBauto">{account.name} - Address</h2>
+                <QRCode className="mB50" size={512} value={account.address} />
+                <div className="flex mBauto">
+                    <p className="body2 mL20">{account.address}</p>
+                    <CopyButton className="mL20" value={account.address} />
+                </div>
+            </PageLayout.Container>
+        </PageLayout>
+    );
+}
