19ccf3d4fa8b1365e7a349051dc205b8b126532f
Søren Bruus Zeppelin <sz@concordium.com>
2021-07-02 09:36:37 +0200

    added inline input field to identity view

--- app/components/IdentityCard/IdentityCard.tsx
+++ app/components/IdentityCard/IdentityCard.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import clsx from 'clsx';
 import PendingImage from '@resources/svg/pending-small.svg';
 import SuccessImage from '@resources/svg/success-small.svg';
 import RejectedImage from '@resources/svg/warning-small.svg';
+import EditIcon from '@resources/svg/edit.svg';
 import { Identity, IdentityObject, IdentityStatus } from '~/utils/types';
 import { formatDate } from '~/utils/timeHelpers';
 import Card from '~/cross-app-components/Card';
@@ -13,8 +14,15 @@ import {
     formatAttributeValue,
     compareAttributes,
 } from '~/utils/identityHelpers';
+import Form from '../Form';
+import Button from '~/cross-app-components/Button';
 
 import styles from './IdentityCard.module.scss';
+import { useUpdateEffect } from '~/utils/hooks';
+
+interface EditIdentityForm {
+    name: string;
+}
 
 interface Props {
     identity: Identity;
@@ -22,6 +30,10 @@ interface Props {
     onClick?: () => void;
     active?: boolean;
     showAttributes?: boolean | AttributeKey[];
+    /**
+     * If true, allows editing name of identity inline. Defaults to false.
+     */
+    canEditName?: boolean;
 }
 
 // Returns the image corresponding to the given status.
@@ -38,6 +50,8 @@ function statusImage(status: IdentityStatus) {
     }
 }
 
+const buttonContent = <EditIcon />;
+
 /**
  * Displays the information of the Identity.
  */
@@ -47,11 +61,22 @@ function IdentityListElement({
     className,
     active = false,
     showAttributes = false,
+    canEditName = false,
 }: Props): JSX.Element {
+    const [isEditing, setIsEditing] = useState(false);
     const identityProvider = JSON.parse(identity.identityProvider);
     const identityObject: IdentityObject | null = JSON.parse(
         identity.identityObject
     )?.value;
+
+    function handleSubmit({ name }: EditIdentityForm) {
+        console.log(name);
+        setIsEditing(false);
+    }
+
+    useUpdateEffect(() => {
+        setIsEditing(false);
+    }, [identity]);
 
     return (
         <Card
@@ -80,8 +105,45 @@ function IdentityListElement({
                         Identity
                     </span>
                 </div>
-
-                <h1> {identity.name} </h1>
+                <Form<EditIdentityForm>
+                    onSubmit={handleSubmit}
+                    className={styles.form}
+                >
+                    <h1 className={styles.name}>
+                        {isEditing ? (
+                            <Form.InlineInput
+                                name="name"
+                                className={styles.nameField}
+                                defaultValue={identity.name}
+                                fallbackValue={identity.name}
+                                autoFocus
+                                rules={{ required: true }}
+                            />
+                        ) : (
+                            <span
+                                style={{
+                                    marginRight: canEditName ? 2 : undefined, // To align as good as possible with input in edit mode.
+                                }}
+                            >
+                                {identity.name}
+                            </span>
+                        )}
+                    </h1>
+                    {canEditName &&
+                        (isEditing ? (
+                            <Form.Submit className={styles.edit} clear>
+                                {buttonContent}
+                            </Form.Submit>
+                        ) : (
+                            <Button
+                                className={styles.edit}
+                                clear
+                                onClick={() => setIsEditing(true)}
+                            >
+                                {buttonContent}
+                            </Button>
+                        ))}
+                </Form>
                 <div className="textFaded">
                     {' '}
                     {identityObject
