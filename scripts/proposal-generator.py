094c9327e1d4748a31316fbadbc8e95daeddb002
Christian Matt <cm@concordium.com>
2021-07-02 16:55:29 +0200

    Use explicit main function.

--- scripts/proposal-generator.py
+++ scripts/proposal-generator.py
@@ -28,15 +28,6 @@ csvDelimiter = ','
 thousandsSep = ','
 expiry = datetime.now() + relativedelta(hours =+ 2) # proposal expires 2 hours from now
 
-if len(sys.argv) != 2:
-	print("Error: Incorrect number of arguments. Please provide the path to a csv file as the only argument.")
-	sys.exit(1)
-
-csvFileName = sys.argv[1]
-
-# Extract base name of csv file without extension and path. Outpul files will contain this name.
-baseCsvName = os.path.splitext(os.path.basename(csvFileName))[0]
-
 # Parses and validates the the amount.
 # Validates that it has at most 6 decimals, is a positive number and 
 # that it fits within a uint64. Parses the amount into its µGTU representation.
@@ -59,89 +50,103 @@ def parse_and_validate_amount(amountString: str, rowNumber: int):
 		sys.exit(2)
 	return amount
 
-# read csv file
-rowNumber = 0
+# Main function
+def main():
+	if len(sys.argv) != 2:
+		print("Error: Incorrect number of arguments. Please provide the path to a csv file as the only argument.")
+		sys.exit(1)
 
-try:
-	with open(csvFileName, newline='', encoding='utf-8-sig') as csvfile:
-		reader = csv.reader(csvfile, delimiter=csvDelimiter)
+	csvFileName = sys.argv[1]
 
-		for row in reader:
-			rowNumber += 1
+	# Extract base name of csv file without extension and path. Outpul files will contain this name.
+	baseCsvName = os.path.splitext(os.path.basename(csvFileName))[0]
 
-			if len(row) != 4:
-				print("Error: Incorrect file format. Each row must contains exactly 4 entires. Row ", rowNumber, " contains ", len(row), ".", sep='')
-				sys.exit(2)
+	# read csv file
+	rowNumber = 0
 
-			senderAddress = row[0]
-			try:
-				b58decode_check(senderAddress)
-			except:
-				print("Encountered an invalid sender address: \"" + senderAddress + "\" at row " + str(rowNumber))
-				sys.exit(2)
+	try:
+		with open(csvFileName, newline='', encoding='utf-8-sig') as csvfile:
+			reader = csv.reader(csvfile, delimiter=csvDelimiter)
 
-			receiverAddress = row[1]
-			try:
-				b58decode_check(receiverAddress)
-			except:
-				print("Encountered an invalid receiver address: \"" + receiverAddress + "\" at row " + str(rowNumber))
-				sys.exit(2)
+			for row in reader:
+				rowNumber += 1
 
-			# Remove thousands separator and trailing/leading whitespaces (if any)
-			initialAmountInput = row[2].replace(thousandsSep, '').strip()
-			initialAmount = parse_and_validate_amount(initialAmountInput, rowNumber)
-			remAmountRawInput = row[3].replace(thousandsSep, '').strip()
-			remAmount = parse_and_validate_amount(remAmountRawInput, rowNumber)
+				if len(row) != 4:
+					print("Error: Incorrect file format. Each row must contains exactly 4 entires. Row ", rowNumber, " contains ", len(row), ".", sep='')
+					sys.exit(2)
 
-			proposal = {
-				"sender": senderAddress,
-				"nonce": "", # filled by desktop wallet
-				"energyAmount": "", # filled by desktop wallet
-				"estimatedFee": "", # filled by desktop wallet,
-				"expiry": {
-					"@type": "bigint",
-					"value": int(expiry.timestamp())
-				},
-				"transactionKind": 19,
-				"payload": {
-					"toAddress": receiverAddress,
-					"schedule": [] # filled below
-				},
-				"signatures": {}
-			}
+				senderAddress = row[0]
+				try:
+					b58decode_check(senderAddress)
+				except:
+					print("Encountered an invalid sender address: \"" + senderAddress + "\" at row " + str(rowNumber))
+					sys.exit(2)
 
-			schedule = [{ # start with initial release and add remaining releases below
-				"amount": initialAmount,
-				"timestamp": int(initialReleaseTime.timestamp()) * 1000 # multiply with 1000 to convert to milliseconds
-			}] 
+				receiverAddress = row[1]
+				try:
+					b58decode_check(receiverAddress)
+				except:
+					print("Encountered an invalid receiver address: \"" + receiverAddress + "\" at row " + str(rowNumber))
+					sys.exit(2)
 
-			# in each remaining step give fraction of amount, rounded down
-			# potentially give more in last release
-			stepAmount = remAmount // (numReleases - 1)
-			lastAmount = remAmount - (numReleases - 2) * stepAmount
+				# Remove thousands separator and trailing/leading whitespaces (if any)
+				initialAmountInput = row[2].replace(thousandsSep, '').strip()
+				initialAmount = parse_and_validate_amount(initialAmountInput, rowNumber)
+				remAmountRawInput = row[3].replace(thousandsSep, '').strip()
+				remAmount = parse_and_validate_amount(remAmountRawInput, rowNumber)
 
-			rTime = firstRemReleaseTime
+				proposal = {
+					"sender": senderAddress,
+					"nonce": "", # filled by desktop wallet
+					"energyAmount": "", # filled by desktop wallet
+					"estimatedFee": "", # filled by desktop wallet,
+					"expiry": {
+						"@type": "bigint",
+						"value": int(expiry.timestamp())
+					},
+					"transactionKind": 19,
+					"payload": {
+						"toAddress": receiverAddress,
+						"schedule": [] # filled below
+					},
+					"signatures": {}
+				}
 
-			for i in range(numReleases - 2) :
-				release = {"amount": stepAmount, "timestamp": int(rTime.timestamp()) * 1000}
-				schedule.append(release)
-				rTime += relativedelta(months =+ 1) # next release is 1 month later
-			
-			lastRelease = {"amount": lastAmount, "timestamp": int(rTime.timestamp()) * 1000}
-			schedule.append(lastRelease)
+				schedule = [{ # start with initial release and add remaining releases below
+					"amount": initialAmount,
+					"timestamp": int(initialReleaseTime.timestamp()) * 1000 # multiply with 1000 to convert to milliseconds
+				}]
 
-			proposal["payload"]["schedule"] = schedule
+				# in each remaining step give fraction of amount, rounded down
+				# potentially give more in last release
+				stepAmount = remAmount // (numReleases - 1)
+				lastAmount = remAmount - (numReleases - 2) * stepAmount
 
-			outFileName = "pre-proposal_" + baseCsvName + "_" + str(rowNumber).zfill(3) + ".json";
-			try:
-				with open(outFileName, 'w') as outFile:
-					json.dump(proposal, outFile, indent=4)
-			except IOError:
-				print("Error writing file \"", outFileName, "\".", sep='')
-				sys.exit(3)
+				rTime = firstRemReleaseTime
 
-except IOError:
-	print("Error reading file \"", csvFileName, "\".", sep='')
-	sys.exit(3)
+				for i in range(numReleases - 2) :
+					release = {"amount": stepAmount, "timestamp": int(rTime.timestamp()) * 1000}
+					schedule.append(release)
+					rTime += relativedelta(months =+ 1) # next release is 1 month later
+				
+				lastRelease = {"amount": lastAmount, "timestamp": int(rTime.timestamp()) * 1000}
+				schedule.append(lastRelease)
 
-print("Successfully generated", rowNumber, "proposals.")
+				proposal["payload"]["schedule"] = schedule
+
+				outFileName = "pre-proposal_" + baseCsvName + "_" + str(rowNumber).zfill(3) + ".json";
+				try:
+					with open(outFileName, 'w') as outFile:
+						json.dump(proposal, outFile, indent=4)
+				except IOError:
+					print("Error writing file \"", outFileName, "\".", sep='')
+					sys.exit(3)
+
+	except IOError:
+		print("Error reading file \"", csvFileName, "\".", sep='')
+		sys.exit(3)
+
+	print("Successfully generated", rowNumber, "proposals.")
+
+if __name__ == "__main__":
+	main()
