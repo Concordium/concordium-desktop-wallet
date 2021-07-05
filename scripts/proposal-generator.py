cd35aad6a040476b3d1e8a53b5a0fa11eb0248df
Christian Matt <cm@concordium.com>
2021-07-05 12:28:21 +0200

    Added functionality for delayed releases.

--- scripts/proposal-generator.py
+++ scripts/proposal-generator.py
@@ -1,10 +1,10 @@
 # This script allows to generate proposals from a csv file exported from Excel.
 # Run the script with one command line argument determining the path to that file.
 # The expected format of that file is a UTF-8 csv file with:
-# one row for each transfer, columns separated by ';'
+# one row for each transfer, columns separated by ','
 # The first column contains sender address, the second one the receiver address
 # The third column contains amount of first release in GTU
-# (as decimal with 6 digits after decimal dot and possibly using '’' as thousands separator)
+# (as decimal with 6 digits after decimal dot and possibly using ',' as thousands separator)
 # The fourth column contains total amount of remaining releases in GTU (formatted as second column)
 # The release schedules are hard-coded in this script.
 #
@@ -17,7 +17,7 @@ import json
 import csv
 import os
 from decimal import *
-from datetime import datetime
+from datetime import datetime,date,time
 from dateutil.relativedelta import relativedelta
 from base58 import b58decode_check
 
@@ -27,6 +27,13 @@ firstRemReleaseTime = datetime.fromisoformat("2021-08-26T14:00:00+01:00")
 csvDelimiter = ','
 thousandsSep = ','
 expiry = datetime.now() + relativedelta(hours =+ 2) # proposal expires 2 hours from now
+
+# If regular releases are before earliestReleaseTime, they get combined into one at that time.
+# earliestReleaseTime = 14:00 CET tomorrow.
+earliestReleaseTime = datetime.combine(date.today(), time.fromisoformat("14:00:00+01:00")) + relativedelta(days =+ 1)
+
+assert initialReleaseTime < firstRemReleaseTime
+
 
 # Parses and validates the the amount.
 # Validates that it has at most 6 decimals, is a positive number and 
@@ -58,12 +65,35 @@ def main():
 
 	csvFileName = sys.argv[1]
 
-	# Extract base name of csv file without extension and path. Outpul files will contain this name.
+	# Extract base name of csv file without extension and path. Output files will contain this name.
 	baseCsvName = os.path.splitext(os.path.basename(csvFileName))[0]
+
+	# Build release schedule.
+	# Normal schedule consists of numReleases, with first one at initialReleaseTime,
+	# and the remaining ones one month after each other, starting with firstRemReleaseTime.
+	#
+	# If the transfer is delayed, some realeases can be in the past. In that case,
+	# combine all releases before earliestReleaseTime into one release at that time.
+	releases = []
+		
+	if initialReleaseTime >= earliestReleaseTime:
+		releases.append(initialReleaseTime)
+	else:
+		releases.append(earliestReleaseTime)
+
+	for i in range(numReleases - 1):
+		# remaining realeses are i month after first remaining release
+		plannedReleaseTime = firstRemReleaseTime + relativedelta(months =+ i)
+
+		# Only add release if not before earliestReleaseTime.
+		# Note that earliestReleaseTime will already be in list since initialReleaseTime < firstRemReleaseTime.
+		if plannedReleaseTime >= earliestReleaseTime:
+			releases.append(plannedReleaseTime)
+	
+	skippedReleases = numReleases - len(releases) # number of releases to be combined into the initial release
 
 	# read csv file
 	rowNumber = 0
-
 	try:
 		with open(csvFileName, newline='', encoding='utf-8-sig') as csvfile:
 			reader = csv.reader(csvfile, delimiter=csvDelimiter)
@@ -112,25 +142,31 @@ def main():
 					"signatures": {}
 				}
 
-				schedule = [{ # start with initial release and add remaining releases below
-					"amount": initialAmount,
-					"timestamp": int(initialReleaseTime.timestamp()) * 1000 # multiply with 1000 to convert to milliseconds
-				}]
+				if len(releases) == 1:
+					# if there is only one release, amount is sum of initial and remaining amount
+					schedule = [{
+						"amount": initialAmount + remAmount,
+						"timestamp": int(releases[0].timestamp()) * 1000 # multiply with 1000 to convert to milliseconds
+					}]
+				else:
+					# if there are more releases, first compute amounts according to original schedule (i.e., assume no skipped releases)
+					# in each remaining step give fraction of amount, rounded down
+					# potentially give more in last release
+					stepAmount = remAmount // (numReleases - 1)
+					lastAmount = remAmount - (numReleases - 2) * stepAmount
 
-				# in each remaining step give fraction of amount, rounded down
-				# potentially give more in last release
-				stepAmount = remAmount // (numReleases - 1)
-				lastAmount = remAmount - (numReleases - 2) * stepAmount
+					schedule = [{ # start with initial release plus all skipped releases and add remaining releases below
+						"amount": initialAmount + (skippedReleases * stepAmount),
+						"timestamp": int(releases[0].timestamp()) * 1000 # multiply with 1000 to convert to milliseconds
+					}]
 
-				rTime = firstRemReleaseTime
+					for i in range(len(releases) - 2) :
+						release = {"amount": stepAmount, "timestamp": int(releases[i+1].timestamp()) * 1000}
+						schedule.append(release)
+					
+					lastRelease = {"amount": lastAmount, "timestamp": int(releases[len(releases)-1].timestamp()) * 1000}
+					schedule.append(lastRelease)
 
-				for i in range(numReleases - 2) :
-					release = {"amount": stepAmount, "timestamp": int(rTime.timestamp()) * 1000}
-					schedule.append(release)
-					rTime += relativedelta(months =+ 1) # next release is 1 month later
-				
-				lastRelease = {"amount": lastAmount, "timestamp": int(rTime.timestamp()) * 1000}
-				schedule.append(lastRelease)
 
 				proposal["payload"]["schedule"] = schedule
 
