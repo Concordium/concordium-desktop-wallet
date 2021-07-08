f2f622b09559f9fd463474bafb7459ed55b4e8b3
tschudid <14889202+tschudid@users.noreply.github.com>
2021-07-08 20:57:08 +0200

    better encapsulation, improved error handling

--- scripts/generator_test.py
+++ scripts/generator_test.py
@@ -1,66 +1,90 @@
 
-proposal_generator = __import__('proposal-generator')
-welcome_proposal_generator = __import__('welcome-proposal-generator')
+pg = __import__('proposal-generator')
+wpg = __import__('welcome-proposal-generator')
 
 from decimal import Decimal
 import unittest
 import random
 
 class TestProposalGenerator(unittest.TestCase):
+ 
+    def format_amount(self,amount_string: str):
+        return amount_string.replace('.',pg.decimalSep).replace(',',pg.thousandsSep)
 
     def test_valid_number(self):
         self.assertEqual(
-            proposal_generator.parse_and_validate_amount('78.123456',1),
-            78123456
+            pg.parse_and_validate_amount(self.format_amount('1278.123456'),1),
+            1278123456
+        )
+        self.assertEqual(
+            pg.parse_and_validate_amount(self.format_amount('1,278.123456'),1),
+            1278123456
         )
 
     def test_random_valid_numbers(self):
         for i in range(0,1000):
-            x = random.randrange(1,proposal_generator.maxAmount)
-            self.assertEqual(
-                proposal_generator.parse_and_validate_amount(str(Decimal(x) / Decimal(1000000)),1),
-                x
-            )
+            x = random.randrange(1,pg.maxAmount//1000000)
+            y = random.randrange(1,1000000)
+            z = x*1000000+y*10**(6-len(str(y)))
+            self.assertEqual(pg.parse_and_validate_amount(self.format_amount(f"{x}.{y}"),1),z)
+            self.assertEqual(pg.parse_and_validate_amount(self.format_amount(f"{x:,}.{y}"),1),z)
 
     def test_invalid_numbers(self):
-        self.assertRaises(ValueError, proposal_generator.parse_and_validate_amount,'-12',1)
-        self.assertRaises(ValueError, proposal_generator.parse_and_validate_amount,'12.',1)
-        self.assertRaises(ValueError, proposal_generator.parse_and_validate_amount,'12,0',1)
-        self.assertRaises(ValueError, proposal_generator.parse_and_validate_amount,'٠.001',1)
-        self.assertRaises(ValueError, proposal_generator.parse_and_validate_amount,'¼',1)
-        self.assertRaises(ValueError, proposal_generator.parse_and_validate_amount,'0',1)
-        self.assertRaises(ValueError, proposal_generator.parse_and_validate_amount,'0.0',1)
-        self.assertRaises(ValueError, proposal_generator.parse_and_validate_amount,'0.0000001',1)
-        self.assertRaises(ValueError, proposal_generator.parse_and_validate_amount,str(proposal_generator.maxAmount),1)
+        #Invalid format
+        self.assertRaises(ValueError, pg.parse_and_validate_amount,self.format_amount('-12'),1)
+        self.assertRaises(ValueError, pg.parse_and_validate_amount,self.format_amount('12,0'),1)
+        self.assertRaises(ValueError, pg.parse_and_validate_amount,self.format_amount('120,0000.0'),1)
+        self.assertRaises(ValueError, pg.parse_and_validate_amount,self.format_amount('0,,,,0,0.1,000,000000'),1)
+        self.assertRaises(ValueError, pg.parse_and_validate_amount,self.format_amount('٠.001'),1)
+        self.assertRaises(ValueError, pg.parse_and_validate_amount,self.format_amount('¼'),1)
+        self.assertRaises(ValueError, pg.parse_and_validate_amount,self.format_amount('0.0000001'),1)
+        #Too small
+        self.assertRaises(ValueError, pg.parse_and_validate_amount,self.format_amount('0'),1)
+        self.assertRaises(ValueError, pg.parse_and_validate_amount,self.format_amount('0.0'),1)
+        #Too large
+        self.assertRaises(ValueError, pg.parse_and_validate_amount,self.format_amount(str(pg.maxAmount//1000000+1)),1)
+
 
 
 class TestWelcomeProposalGenerator(unittest.TestCase):
 
+    def format_amount(self,amount_string: str):
+        return amount_string.replace('.',wpg.decimalSep).replace(',',wpg.thousandsSep)
+
     def test_valid_number(self):
         self.assertEqual(
-            welcome_proposal_generator.parse_and_validate_amount('78.123456',1),
-            78123456
+            wpg.parse_and_validate_amount(self.format_amount('1278.123456'),1),
+            1278123456
+        )
+        self.assertEqual(
+            wpg.parse_and_validate_amount(self.format_amount('1,278.123456'),1),
+            1278123456
         )
 
     def test_random_valid_numbers(self):
         for i in range(0,1000):
-            x = random.randrange(1,proposal_generator.maxAmount)
-            self.assertEqual(
-                welcome_proposal_generator.parse_and_validate_amount(str(Decimal(x) / Decimal(1000000)),1),
-                x
-            )
+            x = random.randrange(1,pg.maxAmount//1000000)
+            y = random.randrange(1,1000000)
+            z = x*1000000+y*10**(6-len(str(y)))
+            self.assertEqual(wpg.parse_and_validate_amount(self.format_amount(f"{x}.{y}"),1),z)
+            self.assertEqual(wpg.parse_and_validate_amount(self.format_amount(f"{x:,}.{y}"),1),z)
 
     def test_invalid_numbers(self):
-        self.assertRaises(ValueError, welcome_proposal_generator.parse_and_validate_amount,'-12',1)
-        self.assertRaises(ValueError, welcome_proposal_generator.parse_and_validate_amount,'12.',1)
-        self.assertRaises(ValueError, welcome_proposal_generator.parse_and_validate_amount,'12,0',1)
-        self.assertRaises(ValueError, welcome_proposal_generator.parse_and_validate_amount,'٠.001',1)
-        self.assertRaises(ValueError, welcome_proposal_generator.parse_and_validate_amount,'¼',1)
-        self.assertRaises(ValueError, welcome_proposal_generator.parse_and_validate_amount,'0',1)
-        self.assertRaises(ValueError, welcome_proposal_generator.parse_and_validate_amount,'0.0',1)
-        self.assertRaises(ValueError, welcome_proposal_generator.parse_and_validate_amount,'0.0000001',1)
-        self.assertRaises(ValueError, welcome_proposal_generator.parse_and_validate_amount,str(welcome_proposal_generator.maxAmount),1)
+        #Invalid format
+        self.assertRaises(ValueError, wpg.parse_and_validate_amount,self.format_amount('-12'),1)
+        self.assertRaises(ValueError, wpg.parse_and_validate_amount,self.format_amount('12,0'),1)
+        self.assertRaises(ValueError, wpg.parse_and_validate_amount,self.format_amount('120,0000.0'),1)
+        self.assertRaises(ValueError, wpg.parse_and_validate_amount,self.format_amount('0,,,,0,0.1,000,000000'),1)
+        self.assertRaises(ValueError, wpg.parse_and_validate_amount,self.format_amount('٠.001'),1)
+        self.assertRaises(ValueError, wpg.parse_and_validate_amount,self.format_amount('¼'),1)
+        self.assertRaises(ValueError, wpg.parse_and_validate_amount,self.format_amount('0.0000001'),1)
+        #Too small
+        self.assertRaises(ValueError, wpg.parse_and_validate_amount,self.format_amount('0'),1)
+        self.assertRaises(ValueError, wpg.parse_and_validate_amount,self.format_amount('0.0'),1)
+        #Too large
+        self.assertRaises(ValueError, wpg.parse_and_validate_amount,self.format_amount(str(wpg.maxAmount//1000000+1)),1)
 
 
 if __name__ == '__main__':
     unittest.main()
+    
