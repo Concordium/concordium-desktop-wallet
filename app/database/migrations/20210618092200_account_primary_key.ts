53efe79a4bc24a4c6e2d1c88f1dd8ac2a1184a76
Hjort <shj@concordium.com>
2021-07-14 09:40:59 +0200

    Merged migrations which updates account table (address is primary, maxTransactionId is string)

--- app/database/migrations/20210618092200_account_primary_key.ts
+++ app/database/migrations/20210618092200_account_primary_key.ts
@@ -1,99 +1,99 @@
 import { Knex } from 'knex';
-import { accountsTable, identitiesTable } from '~/constants/databaseNames.json';
+import {
+    accountsTable,
+    identitiesTable,
+    transactionTable,
+} from '~/constants/databaseNames.json';
 import { ADDRESS_LENGTH } from '~/utils/accountHelpers';
 import { Account } from '~/utils/types';
 
 const TEMP_NAME = 'accounts_temp';
+const insertChunkSize = 100;
 
-function ensureUniqueAccountAddresses(accounts: Account[]): Account[] {
-    return accounts.map((a, i) => ({ ...a, address: a.address || `${i}` }));
+// stringifies MaxTransactionId and ensures unique Account Address
+function upAccounts(accounts: Account[]): Account[] {
+    return accounts.map((a, i) => ({
+        ...a,
+        maxTransactionId: '0',
+        address: a.address || `${i}`,
+    }));
+}
+
+function createCommonFields(table: Knex.CreateTableBuilder) {
+    table
+        .integer('identityId')
+        .references('id')
+        .inTable(identitiesTable)
+        .notNullable();
+    table.string('name');
+    table.string('status');
+    table.integer('signatureThreshold');
+    table.string('incomingAmounts').defaultTo('[]');
+    table.string('selfAmounts').defaultTo('');
+    table.string('totalDecrypted').defaultTo('');
+    table.boolean('allDecrypted').defaultTo(true);
+    table.string('deploymentTransactionId').defaultTo('0');
+    table.boolean('isInitial').defaultTo(false);
+    table.string('rewardFilter').defaultTo('[]');
 }
 
 export async function up(knex: Knex): Promise<void> {
-    await knex.raw('PRAGMA foreign_keys=off;');
-
     await knex.transaction(async (t) => {
-        await t.schema.renameTable(accountsTable, TEMP_NAME);
-        await t.schema.createTable(accountsTable, (table) => {
-            table
-                .integer('identityId')
-                .references('id')
-                .inTable(identitiesTable)
-                .notNullable();
-            table.string('name');
-            table.string('status');
+        await t.schema.createTable(TEMP_NAME, (table) => {
+            createCommonFields(table);
             table.string('address').primary();
-            table.integer('signatureThreshold');
-            table.string('incomingAmounts').defaultTo('[]');
-            table.string('selfAmounts').defaultTo('');
-            table.string('totalDecrypted').defaultTo('');
-            table.boolean('allDecrypted').defaultTo(true);
-            table.integer('maxTransactionId').defaultTo(0);
-            table.string('deploymentTransactionId');
-            table.boolean('isInitial').defaultTo(false);
-            table.string('rewardFilter').defaultTo('[]');
+            table.string('maxTransactionId').defaultTo('0');
         });
 
-        const accounts: Account[] = await t(TEMP_NAME).select();
+        const accounts: Account[] = await t(accountsTable).select();
 
         if (accounts.length) {
             await t.batchInsert(
-                accountsTable,
-                ensureUniqueAccountAddresses(accounts),
-                100
+                TEMP_NAME,
+                upAccounts(accounts),
+                insertChunkSize
             );
         }
+        await t.table(transactionTable).del();
 
-        await knex.schema.dropTableIfExists(TEMP_NAME);
+        await t.schema.dropTableIfExists(accountsTable);
+        await t.schema.renameTable(TEMP_NAME, accountsTable);
     });
-
-    await knex.raw('PRAGMA foreign_keys=on;');
 }
 
-function revertTempAddresses(accounts: Account[]): Account[] {
+type AccountWithNumberMaxTransactionId = Omit<Account, 'maxTransactionId'>;
+
+function downAccounts(
+    accounts: Account[]
+): AccountWithNumberMaxTransactionId[] {
     return accounts.map((a) => ({
         ...a,
+        maxTransactionId: 0,
         address: a.address.length !== ADDRESS_LENGTH ? '' : a.address,
     }));
 }
 
 export async function down(knex: Knex): Promise<void> {
-    await knex.raw('PRAGMA foreign_keys=off;');
-
     await knex.transaction(async (t) => {
-        await t.schema.renameTable(accountsTable, TEMP_NAME);
-        await t.schema.createTable(accountsTable, (table) => {
-            table
-                .integer('identityId')
-                .references('id')
-                .inTable(identitiesTable)
-                .notNullable();
-            table.string('name');
-            table.string('status');
+        await t.schema.createTable(TEMP_NAME, (table) => {
+            createCommonFields(table);
             table.string('address');
-            table.integer('signatureThreshold');
-            table.string('incomingAmounts').defaultTo('[]');
-            table.string('selfAmounts').defaultTo('');
-            table.string('totalDecrypted').defaultTo('');
-            table.boolean('allDecrypted').defaultTo(true);
             table.integer('maxTransactionId').defaultTo(0);
-            table.string('deploymentTransactionId');
-            table.boolean('isInitial').defaultTo(false);
-            table.string('rewardFilter').defaultTo('[]');
         });
 
-        const accounts: Account[] = await t(TEMP_NAME).select();
+        const accounts: Account[] = await t(accountsTable).select();
 
         if (accounts.length) {
             await t.batchInsert(
-                accountsTable,
-                revertTempAddresses(accounts),
-                100
+                TEMP_NAME,
+                downAccounts(accounts),
+                insertChunkSize
             );
         }
 
-        await knex.schema.dropTableIfExists(TEMP_NAME);
-    });
+        await t.table(transactionTable).del();
 
-    await knex.raw('PRAGMA foreign_keys=on;');
+        await t.schema.dropTableIfExists(accountsTable);
+        await t.schema.renameTable(TEMP_NAME, accountsTable);
+    });
 }
