4da9703e61228ae3067341cd5d4f3142154fd08d
Søren Bruus Zeppelin <sz@concordium.com>
2021-08-11 13:02:03 +0200

    animate notifications

--- app/shell/Notifications/Notifications.tsx
+++ app/shell/Notifications/Notifications.tsx
@@ -1,4 +1,5 @@
-import React, { Fragment } from 'react';
+import { AnimatePresence, motion, Transition, Variants } from 'framer-motion';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Notification from '~/components/Notification';
 import Portal from '~/cross-app-components/Portal';
@@ -10,6 +11,21 @@ import { RootState } from '~/store/store';
 
 import styles from './Notifications.module.scss';
 
+const transition: Transition = {
+    ease: 'easeIn',
+    duration: 0.2,
+};
+
+const transitionVariants: Variants = {
+    initial: { opacity: 0, transform: 'translate(100%, 0)' },
+    enter: { opacity: 1, transform: 'translate(0, 0)' },
+    exit: {
+        opacity: 0,
+        transform: 'translate(0, -100%)',
+        transition: { ease: 'easeOut' },
+    },
+};
+
 export default function Notifications() {
     const { notifications } = useSelector((s: RootState) => s.notification);
     const dispatch = useDispatch();
@@ -20,23 +36,33 @@ export default function Notifications() {
 
     return (
         <Portal className={styles.root}>
-            {notifications.map((n) => (
-                <Fragment key={n.id}>
-                    {n.level === NotificationLevel.Update ? (
-                        <Notification
-                            level={n.level}
-                            onCloseClick={() => handleClose(n.id)}
-                        /> // TODO: Replace with update notification
-                    ) : (
-                        <Notification
-                            level={n.level}
-                            onCloseClick={() => handleClose(n.id)}
-                        >
-                            {n.message}
-                        </Notification>
-                    )}
-                </Fragment>
-            ))}
+            <AnimatePresence>
+                {notifications.map((n) => (
+                    <motion.div
+                        key={n.id}
+                        transition={transition}
+                        variants={transitionVariants}
+                        initial="initial"
+                        animate="enter"
+                        exit="exit"
+                    >
+                        {n.level === NotificationLevel.Update ? (
+                            // TODO: Replace with update notification
+                            <Notification
+                                level={n.level}
+                                onCloseClick={() => handleClose(n.id)}
+                            />
+                        ) : (
+                            <Notification
+                                level={n.level}
+                                onCloseClick={() => handleClose(n.id)}
+                            >
+                                {n.message}
+                            </Notification>
+                        )}
+                    </motion.div>
+                ))}
+            </AnimatePresence>
         </Portal>
     );
 }
