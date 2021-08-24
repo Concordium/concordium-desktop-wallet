900428430f53f77fa15440d14efedef8e2d6a350
Hjort <shj@concordium.com>
2021-08-24 12:49:41 +0200

    Added mail + text to failed identity

--- app/components/IdentityCard/FailedIdentityDetails.tsx
+++ app/components/IdentityCard/FailedIdentityDetails.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import clsx from 'clsx';
 import { Identity } from '~/utils/types';
 import SidedRow from '~/components/SidedRow';
 import CopyButton from '~/components/CopyButton';
+import ExternalLink from '~/components/ExternalLink';
 import { getSessionId } from '~/utils/identityHelpers';
 
 import styles from './IdentityCard.module.scss';
@@ -14,12 +16,22 @@ export default function FailedIdentityDetails({
     identity,
 }: FailedIdentityDetailsProps) {
     const sessionId = getSessionId(identity);
+    const mail = 'concordium-idiss@notabene.id';
+    const subject = `Reference: ${sessionId}`;
 
     return (
         <div className={styles.details}>
+            <p>
+                Contact{' '}
+                <ExternalLink href={`mailto:${mail}?subject=${subject}`}>
+                    {mail}
+                </ExternalLink>{' '}
+                for support.
+            </p>
+            <p>Please provide the reference, when doing so.</p>
             <SidedRow
-                className={styles.detailsRow}
-                left={`SessionId: ${sessionId.substring(0, 8)}...`}
+                className={clsx('body2', styles.detailsRow)}
+                left={`Reference: ${sessionId.substring(0, 8)}...`}
                 right={<CopyButton value={sessionId} />}
             />
         </div>
