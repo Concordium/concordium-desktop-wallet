2d9efbf73e4f2d50c7fc65bc072f3ecfb86fc397
Søren Bruus Zeppelin <sz@concordium.com>
2021-08-31 14:25:46 +0200

    move load accounts call up

--- app/pages/Accounts/AccountPage.tsx
+++ app/pages/Accounts/AccountPage.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Dispatch } from 'redux';
+import { push } from 'connected-react-router';
 
 import NoIdentities from '~/components/NoIdentities';
-import { accountsSelector } from '~/features/AccountSlice';
+import {
+    accountsSelector,
+    loadAccountInfos,
+    loadAccounts,
+} from '~/features/AccountSlice';
 import MasterDetailPageLayout from '~/components/MasterDetailPageLayout';
 import { RootState } from '~/store/store';
+import SimpleErrorModal from '~/components/SimpleErrorModal';
+import routes from '~/constants/routes.json';
 
 import useAccountSync from './useAccountSync';
 import AccountListPage from './AccountListPage';
@@ -12,10 +20,22 @@ import AccountDetailsPage from './AccountDetailsPage';
 
 const { Header } = MasterDetailPageLayout;
 
+async function load(dispatch: Dispatch) {
+    const accounts = await loadAccounts(dispatch);
+    return loadAccountInfos(accounts, dispatch);
+}
+
 export default function AccountsPage() {
     const accounts = useSelector(accountsSelector);
+    const dispatch = useDispatch();
     useAccountSync();
     const { simpleView } = useSelector((s: RootState) => s.accounts);
+    const [error, setError] = useState<string>();
+
+    useEffect(() => {
+        load(dispatch).catch((e: Error) => setError(e.message));
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [dispatch]);
 
     if (accounts.length === 0) {
         return (
@@ -28,9 +48,15 @@ export default function AccountsPage() {
         );
     }
 
-    if (simpleView) {
-        return <AccountListPage />;
-    }
-
-    return <AccountDetailsPage />;
+    return (
+        <>
+            <SimpleErrorModal
+                show={Boolean(error)}
+                header="Unable to load Accounts"
+                content={error}
+                onClick={() => dispatch(push(routes.HOME))}
+            />
+            {simpleView ? <AccountListPage /> : <AccountDetailsPage />}
+        </>
+    );
 }
