40f661a05eec23b6e170ccd639ff104e53da2199
Søren Bruus Zeppelin <sz@concordium.com>
2021-09-02 11:29:08 +0200

    transaction group stories

--- app/pages/Accounts/TransactionList/stories/TransactionListElement.stories.tsx
+++ app/pages/Accounts/TransactionList/stories/TransactionListElement.stories.tsx
@@ -1,60 +1,28 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react/types-6-0';
 
-import {
-    Account,
-    PropsOf,
-    TransactionKindString,
-    TransactionStatus,
-    TransferTransaction,
-} from '~/utils/types';
+import { PropsOf } from '~/utils/types';
 import TransactionListElement from '../TransactionListElement';
-import { StoreWrapper } from '~/store/store';
-import { AccountState } from '~/features/AccountSlice';
-import { microGTUPerGTU } from '~/utils/gtu';
-import { ElementStoryComponent } from './deps';
+import {
+    account,
+    ElementStoryComponent,
+    StoryContext,
+    transactionBase,
+} from './deps';
 
 export default {
     title: 'Account Page/Transaction List/Element',
     component: ElementStoryComponent,
 } as Meta;
 
-const account: Account = {
-    address: '123',
-} as Account;
-
 const Template: Story<PropsOf<typeof ElementStoryComponent>> = ({
     showDate,
     ...transaction
 }) => (
-    <StoreWrapper
-        accounts={
-            {
-                accounts: [account],
-                chosenAccountAddress: account.address,
-            } as AccountState
-        }
-    >
-        <div style={{ width: 428 }}>
-            <TransactionListElement
-                showDate={showDate}
-                transaction={transaction}
-            />
-        </div>
-    </StoreWrapper>
+    <StoryContext>
+        <TransactionListElement showDate={showDate} transaction={transaction} />
+    </StoryContext>
 );
-
-const transactionBase: TransferTransaction = {
-    toAddress: account.address,
-    fromAddress: 'Hjku41290updfaæljsDA',
-    blockTime: `${Date.now() / 1000}`,
-    subtotal: `${Number(100n * microGTUPerGTU)}`,
-    cost: `${Number(123n * microGTUPerGTU) / 1000}`,
-    blockHash: 'æk13h2æ4ljh1234h',
-    status: TransactionStatus.Finalized,
-    transactionKind: TransactionKindString.Transfer,
-    transactionHash: 'dsæiahfpæi23æklnæ',
-};
 
 export const SimpleTransferIn = Template.bind({});
 SimpleTransferIn.args = {
---
+++ app/pages/Accounts/TransactionList/stories/TransactionListGroup.stories.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react/types-6-0';
+import TransactionListGroup from '../TransactionListGroup';
+import { PropsOf } from '~/utils/types';
+import { StoryContext, transactionBase } from './deps';
+
+export default {
+    title: 'Account Page/Transaction List/Group',
+    component: TransactionListGroup,
+} as Meta;
+
+const Template: Story<PropsOf<typeof TransactionListGroup>> = (args) => (
+    <StoryContext>
+        <TransactionListGroup {...args} />
+    </StoryContext>
+);
+
+export const SingleGroup = Template.bind({});
+SingleGroup.args = {
+    header: 'Today',
+    transactions: [
+        transactionBase,
+        transactionBase,
+        transactionBase,
+        transactionBase,
+    ],
+};
+
+const MultiGroupTemplate: Story<PropsOf<typeof TransactionListGroup>> = (
+    args
+) => (
+    <StoryContext>
+        <TransactionListGroup {...args} />
+        <TransactionListGroup {...args} />
+        <TransactionListGroup {...args} />
+        <TransactionListGroup {...args} />
+    </StoryContext>
+);
+
+export const MultipleGroups = MultiGroupTemplate.bind({});
+MultipleGroups.args = {
+    header: 'Today',
+    transactions: [
+        transactionBase,
+        transactionBase,
+        transactionBase,
+        transactionBase,
+    ],
+};
