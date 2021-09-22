52e79ec970b79e146433e5823c9df47ab2b88416
Søren Bruus Zeppelin <sz@concordium.com>
2021-09-22 15:45:42 +0200

    start abortcontroller

--- app/pages/Accounts/TransactionList/InfiniteTransactionList.tsx
+++ app/pages/Accounts/TransactionList/InfiniteTransactionList.tsx
@@ -96,6 +96,7 @@ export default function InfiniteTransactionList({
             return;
         }
 
+        loadController.start();
         await dispatch(
             loadTransactions({
                 controller: loadController,
