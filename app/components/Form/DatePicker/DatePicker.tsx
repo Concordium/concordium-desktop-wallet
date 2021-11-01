b6a0b1f0c15b10a3a7a23553d7f30539804dfcca
Søren Bruus Zeppelin <sz@concordium.com>
2021-11-01 10:45:03 +0100

    inherit shared input styles

--- app/components/Form/DatePicker/DatePicker.tsx
+++ app/components/Form/DatePicker/DatePicker.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDatePicker from 'react-datepicker';
+import Label from '~/components/Label';
 import { CommonInputProps } from '../common';
+
+import styles from './DatePicker.module.scss';
 
 interface Props extends CommonInputProps {
     value: Date | undefined;
@@ -8,17 +11,20 @@ interface Props extends CommonInputProps {
     onBlur?(): void;
 }
 
-export default function DatePicker({ value, onChange, onBlur }: Props) {
+export default function DatePicker({ value, onChange, onBlur, label }: Props) {
     return (
-        <ReactDatePicker
-            selected={value}
-            onChange={onChange}
-            onBlur={onBlur}
-            showTimeSelect
-            timeCaption="Time"
-            timeFormat="HH:mm"
-            timeIntervals={15}
-            dateFormat="dd-MM-yyyy 'at' HH:mm:ss"
-        />
+        <div className={styles.root}>
+            {label && <Label>{label}</Label>}
+            <ReactDatePicker
+                selected={value}
+                onChange={onChange}
+                onBlur={onBlur}
+                showTimeSelect
+                timeCaption="Time"
+                timeFormat="HH:mm"
+                timeIntervals={15}
+                dateFormat="dd-MM-yyyy 'at' HH:mm:ss"
+            />
+        </div>
     );
 }
