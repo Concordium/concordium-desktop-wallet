550d75f548c5b788ff43da22485f851ea42c7e79
Hjort <shj@concordium.com>
2021-12-16 16:20:07 +0100

    Place ErrorBoundary around Root + catch errors in initialization (index.ts)

--- app/components/ErrorBoundary.tsx
+++ app/components/ErrorBoundary.tsx
@@ -30,6 +30,7 @@ export default class ErrorBoundary extends Component<
     componentDidCatch(error: Error, errorInfo: ErrorInfo) {
         // You can also log the error to an error reporting service
         // eslint-disable-next-line no-console
+        window.log.error(error);
         console.error(error, errorInfo);
     }
 
--- app/index.tsx
+++ app/index.tsx
@@ -7,40 +7,49 @@ import Root from './shell/Root';
 import { history, configuredStore } from './store/store';
 import { init as initMisc } from './features/MiscSlice';
 import { triggerUpdateNotification } from './features/NotificationSlice';
+import ErrorBoundary from '~/components/ErrorBoundary';
 
 import './styles/app.global.scss';
 
-const store = configuredStore();
+try {
+    const store = configuredStore();
 
-window.autoUpdate.onError((_, errorMessage: string, error: Error) =>
-    window.log.error(errorMessage, { error })
-);
+    window.autoUpdate.onError((_, errorMessage: string, error: Error) =>
+        window.log.error(errorMessage, { error })
+    );
 
-initMisc(store.dispatch);
-window.addListener.openRoute((_, route: string) => {
-    window.log.info(`Routed to${route}`);
-    store.dispatch(push(route));
-});
-// eslint-disable-next-line @typescript-eslint/no-explicit-any, no-console
-window.addListener.logFromMain((_, ...args: any[]) => console.log(...args));
+    initMisc(store.dispatch);
+    window.addListener.openRoute((_, route: string) => {
+        window.log.info(`Routed to${route}`);
+        store.dispatch(push(route));
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, no-console
+    window.addListener.logFromMain((_, ...args: any[]) => console.log(...args));
 
-window.addEventListener('unhandledrejection', (promiseRejectionEvent) =>
-    window.log.error('Uncaught rejection: ', {
-        error: promiseRejectionEvent.reason.toString(),
-    })
-);
+    window.addEventListener('unhandledrejection', (promiseRejectionEvent) =>
+        window.log.error('Uncaught rejection: ', {
+            error: promiseRejectionEvent.reason.toString(),
+        })
+    );
 
-window.autoUpdate.onUpdateAvailable((_, info: UpdateInfo) => {
-    store.dispatch(triggerUpdateNotification(info.version));
-});
+    window.autoUpdate.onUpdateAvailable((_, info: UpdateInfo) => {
+        store.dispatch(triggerUpdateNotification(info.version));
+    });
 
-const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer;
+    const AppContainer = process.env.PLAIN_HMR
+        ? Fragment
+        : ReactHotAppContainer;
 
-document.addEventListener('DOMContentLoaded', () =>
-    render(
-        <AppContainer>
-            <Root store={store} history={history} />
-        </AppContainer>,
-        document.getElementById('root')
-    )
-);
+    document.addEventListener('DOMContentLoaded', () =>
+        render(
+            <AppContainer>
+                <ErrorBoundary>
+                    <Root store={store} history={history} />
+                </ErrorBoundary>
+            </AppContainer>,
+            document.getElementById('root')
+        )
+    );
+} catch (error) {
+    window.log.error('Error thrown in index.tsx', { error });
+}
