f8dc5899da8f09fbf492e6ac09237cae0f78b422
Søren Bruus Zeppelin <sz@concordium.com>
2022-02-21 12:04:18 +0100

    singleton commission rate

--- app/components/Transfers/configureBaker/CommissionsPage.tsx
+++ app/components/Transfers/configureBaker/CommissionsPage.tsx
@@ -1,7 +1,9 @@
+import { RewardFraction } from '@concordium/node-sdk';
 import React, { useCallback } from 'react';
 import { useSelector } from 'react-redux';
 
 import Form from '~/components/Form';
+import Label from '~/components/Label';
 import { MultiStepFormPageProps } from '~/components/MultiStepForm';
 import { accountInfoSelector } from '~/features/AccountSlice';
 import { toFixed } from '~/utils/numberStringHelpers';
@@ -11,6 +13,7 @@ import {
 } from '~/utils/rewardFractionHelpers';
 import {
     Commissions,
+    ConfigureBakerFlowState,
     getDefaultCommissions,
     getExistingBakerValues,
 } from '~/utils/transactionFlows/configureBaker';
@@ -50,8 +53,57 @@ const formatCommission = toFixed(3);
 const renderExistingValue = (value: number) =>
     formatCommission(fractionResolutionToPercentage(value).toString());
 
+interface CommissionFieldProps {
+    label: string;
+    name: string;
+    from: RewardFraction;
+    to: RewardFraction;
+    existing: RewardFraction | undefined;
+}
+
+const CommissionField = ({
+    label,
+    name,
+    from,
+    to,
+    existing,
+}: CommissionFieldProps) => {
+    const fromFormatted = fractionResolutionToPercentage(from);
+    const toFormatted = fractionResolutionToPercentage(to);
+
+    if (from === to) {
+        return (
+            <div>
+                <Label className="mB5">{label}</Label>
+                <span className="textFaded body2">{fromFormatted}%</span>
+            </div>
+        );
+    }
+
+    return (
+        <>
+            {existing !== undefined && (
+                <div className="body3 mono mB10">
+                    Current value:
+                    {renderExistingValue(existing)}%
+                </div>
+            )}
+            <Form.Slider
+                label={label}
+                name={name}
+                min={fromFormatted}
+                max={toFormatted}
+                {...commonSliderProps}
+            />
+        </>
+    );
+};
+
 interface CommissionsPageProps
-    extends Omit<MultiStepFormPageProps<Commissions>, 'formValues'> {
+    extends Omit<
+        MultiStepFormPageProps<Commissions, ConfigureBakerFlowState>,
+        'formValues'
+    > {
     account: Account;
 }
 
@@ -67,8 +119,8 @@ export default function CommissionsPage({
         [P in keyof Commissions]: [number, number];
     } = {
         transactionFeeCommission: [5000, 15000],
-        bakingRewardCommission: [5000, 15000],
-        finalizationRewardCommission: [5000, 15000],
+        bakingRewardCommission: [50000, 100000],
+        finalizationRewardCommission: [100000, 100000],
     };
 
     const { commissions: existing } = getExistingBakerValues(accountInfo) ?? {};
@@ -105,60 +157,26 @@ export default function CommissionsPage({
                     When you open your baker as a pool, you have to set
                     commission rates. You can do so below:
                 </p>
-                {existing?.transactionFeeCommission !== undefined && (
-                    <div className="body3 mono mB10">
-                        Current value:
-                        {renderExistingValue(existing.transactionFeeCommission)}
-                        %
-                    </div>
-                )}
-                <Form.Slider
+                <CommissionField
                     label="Transaction fee commissions"
                     name={commissionsFieldNames.transactionFeeCommission}
-                    min={fractionResolutionToPercentage(
-                        boundaries.transactionFeeCommission[0]
-                    )}
-                    max={fractionResolutionToPercentage(
-                        boundaries.transactionFeeCommission[1]
-                    )}
-                    {...commonSliderProps}
+                    from={boundaries.transactionFeeCommission[0]}
+                    to={boundaries.transactionFeeCommission[1]}
+                    existing={existing?.transactionFeeCommission}
                 />
-                {existing?.bakingRewardCommission !== undefined && (
-                    <div className="body3 mono mB10">
-                        Current value:
-                        {renderExistingValue(existing.bakingRewardCommission)}%
-                    </div>
-                )}
-                <Form.Slider
+                <CommissionField
                     label="Baking reward commissions"
                     name={commissionsFieldNames.bakingRewardCommission}
-                    min={fractionResolutionToPercentage(
-                        boundaries.bakingRewardCommission[0]
-                    )}
-                    max={fractionResolutionToPercentage(
-                        boundaries.bakingRewardCommission[1]
-                    )}
-                    {...commonSliderProps}
+                    from={boundaries.bakingRewardCommission[0]}
+                    to={boundaries.bakingRewardCommission[1]}
+                    existing={existing?.bakingRewardCommission}
                 />
-                {existing?.finalizationRewardCommission !== undefined && (
-                    <div className="body3 mono mB10">
-                        Current value:
-                        {renderExistingValue(
-                            existing.finalizationRewardCommission
-                        )}
-                        %
-                    </div>
-                )}
-                <Form.Slider
+                <CommissionField
                     label="Finalization reward commissions"
                     name={commissionsFieldNames.finalizationRewardCommission}
-                    min={fractionResolutionToPercentage(
-                        boundaries.finalizationRewardCommission[0]
-                    )}
-                    max={fractionResolutionToPercentage(
-                        boundaries.finalizationRewardCommission[1]
-                    )}
-                    {...commonSliderProps}
+                    from={boundaries.finalizationRewardCommission[0]}
+                    to={boundaries.finalizationRewardCommission[1]}
+                    existing={existing?.finalizationRewardCommission}
                 />
             </div>
             <Form.Submit className={styles.continue}>Continue</Form.Submit>
