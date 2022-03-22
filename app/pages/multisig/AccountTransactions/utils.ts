d406b9bfd110a3dd3b1a8a09bf4b5255138115ae
Søren Bruus Zeppelin <sz@concordium.com>
2022-03-22 11:52:16 +0100

    ..

--- app/pages/multisig/AccountTransactions/utils.ts
+++ app/pages/multisig/AccountTransactions/utils.ts
@@ -1,9 +1,8 @@
+import { DeepPartial } from '~/utils/types';
+
 /**
  * Takes an object with values, and another object (of the same type) with changes. Returns a function that identifies whether a field should be shown or not.
  */
-
-import { DeepPartial } from 'redux';
-
 // eslint-disable-next-line import/prefer-default-export
 export const shouldShowField = <T, C extends DeepPartial<T>>(
     values: Partial<T>,
