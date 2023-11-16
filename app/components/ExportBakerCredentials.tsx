2e06a3bf110452ac8bf9b7aea089f7c36786556d
Hjort <shj@concordium.com>
2023-11-16 11:32:32 +0100

    Address feedback

--- app/components/ExportBakerCredentials.tsx
+++ app/components/ExportBakerCredentials.tsx
@@ -63,7 +63,7 @@ export default function ExportBakerCredentials({
                         <p className="mT0">
                             Your validator keys have been generated. Before you
                             can continue, you must export and save them. The
-                            keys will have to placed with the validator node.
+                            keys will have to be placed with the validator node.
                         </p>
                         <DisplayPublicKey
                             name="Election verify key:"
