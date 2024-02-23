4ec931ca126d4248908cb516abb2e5fbaf74cf3b
orhoj <jo@concordium.com>
2024-02-23 11:40:07 +0100

    Poll for Ledger status

--- app/preload/ledger/ledgerObserverImpl.ts
+++ app/preload/ledger/ledgerObserverImpl.ts
@@ -27,12 +27,81 @@ export default class LedgerObserverImpl implements LedgerObserver {
         return this.concordiumClient;
     }
 
+    /**
+     * We also poll for the status of the Ledger as we cannot solely rely on the
+     * USB events (they do not work correctly on Windows).
+     */
+    pollForLedgerStatus(mainWindow: EventEmitter) {
+        setTimeout(() => {
+            loggingMethods.info('Polling for a Ledger status');
+            TransportNodeHid.open('descriptor is not used')
+                .then((transport) => {
+                    const localClient = new ConcordiumLedgerClientMain(
+                        mainWindow,
+                        transport
+                    );
+
+                    loggingMethods.info('Connected to a device');
+
+                    localClient
+                        .getAppAndVersion()
+                        .then((appAndVersion) => {
+                            loggingMethods.info(JSON.stringify(appAndVersion));
+                            let action;
+                            if (!appAndVersion) {
+                                // We could not extract the version information.
+                                action = LedgerSubscriptionAction.RESET;
+                            } else if (!isConcordiumApp(appAndVersion)) {
+                                // The device has been connected, but the Concordium application has not
+                                // been opened yet.
+                                action = LedgerSubscriptionAction.PENDING;
+                            } else if (isOutdated(appAndVersion)) {
+                                // The device has been connected, but the Concordium application is outdated
+                                action = LedgerSubscriptionAction.OUTDATED;
+                            } else {
+                                action =
+                                    LedgerSubscriptionAction.CONNECTED_SUBSCRIPTION;
+                            }
+
+                            mainWindow.emit(
+                                ledgerIpcCommands.listenChannel,
+                                action,
+                                'WeNeedDeviceName'
+                            );
+
+                            this.pollForLedgerStatus(mainWindow);
+                            return localClient;
+                        })
+                        .catch(() => {
+                            mainWindow.emit(
+                                ledgerIpcCommands.listenChannel,
+                                LedgerSubscriptionAction.RESET,
+                                'WeNeedDeviceName'
+                            );
+                            this.pollForLedgerStatus(mainWindow);
+                        });
+
+                    return 'hello';
+                })
+                .catch(() => {
+                    mainWindow.emit(
+                        ledgerIpcCommands.listenChannel,
+                        LedgerSubscriptionAction.RESET,
+                        'WeNeedDeviceName'
+                    );
+                    this.pollForLedgerStatus(mainWindow);
+                });
+        }, 5000);
+    }
+
     async subscribeLedger(mainWindow: EventEmitter): Promise<void> {
         if (!this.ledgerSubscription) {
             setupUsbListener();
             this.ledgerSubscription = TransportNodeHid.listen(
                 this.createLedgerObserver(mainWindow)
             );
+
+            this.pollForLedgerStatus(mainWindow);
         }
     }
 
