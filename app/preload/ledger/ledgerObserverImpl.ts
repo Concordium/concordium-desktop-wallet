4fac7a79e14e508cdcbac3e69a0fe8a9f3eef0ba
orhoj <jo@concordium.com>
2024-02-23 14:02:01 +0100

    Use detach event to double check connection

--- app/preload/ledger/ledgerObserverImpl.ts
+++ app/preload/ledger/ledgerObserverImpl.ts
@@ -7,11 +7,26 @@ import type {
     Subscription,
 } from '@ledgerhq/hw-transport';
 import EventEmitter from 'events';
+import { usb } from 'usb';
 import ConcordiumLedgerClientMain from '../../features/ledger/ConcordiumLedgerClientMain';
 import { isConcordiumApp, isOutdated } from '../../components/ledger/util';
 import { LedgerSubscriptionAction } from '../../components/ledger/useLedger';
 import ledgerIpcCommands from '~/constants/ledgerIpcCommands.json';
 import { LedgerObserver } from './ledgerObserver';
+import loggingMethods from '../logging';
+
+// This value has been lifted from @ledger/devices. We do not add the dependency
+// as we solely need this value.
+const ledgerUSBVendorId = 0x2c97;
+
+async function isLedgerConnected(): Promise<boolean> {
+    try {
+        await TransportNodeHid.open('');
+        return true;
+    } catch {
+        return false;
+    }
+}
 
 export default class LedgerObserverImpl implements LedgerObserver {
     concordiumClient: ConcordiumLedgerClientMain | undefined;
@@ -25,18 +40,42 @@ export default class LedgerObserverImpl implements LedgerObserver {
         return this.concordiumClient;
     }
 
+    private onRemoveUsbDevice(device: usb.Device, mainWindow: EventEmitter) {
+        // Ignore events for non-Ledger USB devices.
+        if (device.deviceDescriptor.idVendor !== ledgerUSBVendorId) {
+            return;
+        }
+
+        loggingMethods.info(JSON.stringify(device));
+
+        isLedgerConnected()
+            .then((connected) => {
+                if (connected) {
+                    this.setLedgerState(mainWindow);
+                }
+                return true;
+            })
+            .catch(() => {});
+    }
+
     async subscribeLedger(mainWindow: EventEmitter): Promise<void> {
         if (!this.ledgerSubscription) {
             this.ledgerSubscription = TransportNodeHid.listen(
                 this.createLedgerObserver(mainWindow)
             );
+
+            // The TransportNodeHid.listen() does not always catch all the relevant
+            // USB events on Windows. Therefore we add a raw USB listener as a backup
+            // that will ensure that we maintain an intact connection.
+            usb.on('detach', (event) =>
+                this.onRemoveUsbDevice(event, mainWindow)
+            );
         }
     }
 
     async resetTransport(mainWindow: EventEmitter) {
-        await TransportNodeHid.disconnect();
-        const transport = await TransportNodeHid.open();
-        transport.setAllowAutoDisconnect(false);
+        TransportNodeHid.disconnect();
+        const transport = await TransportNodeHid.open('');
         this.concordiumClient = new ConcordiumLedgerClientMain(
             mainWindow,
             transport
@@ -58,6 +97,56 @@ export default class LedgerObserverImpl implements LedgerObserver {
         }
     }
 
+    private async setLedgerState(
+        mainWindow: EventEmitter,
+        deviceName?: string
+    ) {
+        const transport = await TransportNodeHid.open('');
+        this.concordiumClient = new ConcordiumLedgerClientMain(
+            mainWindow,
+            transport
+        );
+        let appAndVersion;
+        try {
+            appAndVersion = await this.concordiumClient.getAppAndVersion();
+        } catch (e) {
+            throw new Error(`Unable to get current app: ${e}`);
+        }
+        let action;
+        if (!appAndVersion) {
+            // We could not extract the version information.
+            action = LedgerSubscriptionAction.RESET;
+        } else if (!isConcordiumApp(appAndVersion)) {
+            // The device has been connected, but the Concordium application has not
+            // been opened yet.
+            action = LedgerSubscriptionAction.PENDING;
+        } else if (isOutdated(appAndVersion)) {
+            // The device has been connected, but the Concordium application is outdated
+            action = LedgerSubscriptionAction.OUTDATED;
+        } else {
+            action = LedgerSubscriptionAction.CONNECTED_SUBSCRIPTION;
+        }
+        mainWindow.emit(ledgerIpcCommands.listenChannel, action, deviceName);
+    }
+
+    private async onAdd(
+        event: DescriptorEvent<string>,
+        mainWindow: EventEmitter
+    ) {
+        const deviceName = event.deviceModel?.productName;
+        this.setLedgerState(mainWindow, deviceName);
+    }
+
+    private onRemove(mainWindow: EventEmitter) {
+        if (this.concordiumClient) {
+            this.concordiumClient.closeTransport();
+        }
+        mainWindow.emit(
+            ledgerIpcCommands.listenChannel,
+            LedgerSubscriptionAction.RESET
+        );
+    }
+
     /**
      * Creates an observer for events happening on the Ledger. The events will be sent using
      * IPC to the window provided.
@@ -76,50 +165,11 @@ export default class LedgerObserverImpl implements LedgerObserver {
                     LedgerSubscriptionAction.ERROR_SUBSCRIPTION
                 );
             },
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            next: async (event: any) => {
+            next: async (event: DescriptorEvent<string>) => {
                 if (event.type === 'add') {
-                    const deviceName = event.deviceModel.productName;
-                    const transport = await TransportNodeHid.open();
-                    transport.setAllowAutoDisconnect(false);
-                    this.concordiumClient = new ConcordiumLedgerClientMain(
-                        mainWindow,
-                        transport
-                    );
-                    let appAndVersion;
-                    try {
-                        appAndVersion = await this.concordiumClient.getAppAndVersion();
-                    } catch (e) {
-                        throw new Error(`Unable to get current app: ${e}`);
-                    }
-                    let action;
-                    if (!appAndVersion) {
-                        // We could not extract the version information.
-                        action = LedgerSubscriptionAction.RESET;
-                    } else if (!isConcordiumApp(appAndVersion)) {
-                        // The device has been connected, but the Concordium application has not
-                        // been opened yet.
-                        action = LedgerSubscriptionAction.PENDING;
-                    } else if (isOutdated(appAndVersion)) {
-                        // The device has been connected, but the Concordium application is outdated
-                        action = LedgerSubscriptionAction.OUTDATED;
-                    } else {
-                        action =
-                            LedgerSubscriptionAction.CONNECTED_SUBSCRIPTION;
-                    }
-                    mainWindow.emit(
-                        ledgerIpcCommands.listenChannel,
-                        action,
-                        deviceName
-                    );
+                    this.onAdd(event, mainWindow);
                 } else if (event.type === 'remove') {
-                    if (this.concordiumClient) {
-                        this.concordiumClient.closeTransport();
-                    }
-                    mainWindow.emit(
-                        ledgerIpcCommands.listenChannel,
-                        LedgerSubscriptionAction.RESET
-                    );
+                    this.onRemove(mainWindow);
                 }
             },
         };
