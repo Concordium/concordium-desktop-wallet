4e5a04a1dbe81ed74700a67ccd3096662e14b394
Mani <msinghster@gmail.com>
2025-04-24 15:48:04 -0600

    CredentialDao fix

--- app/database/CredentialDao.ts
+++ app/database/CredentialDao.ts
@@ -1,17 +1,44 @@
 import waitForPreloadReady from '../utils/preloadReady';
 
-await waitForPreloadReady();
+let insertCredential: typeof window.database.credentials.insert;
+let removeCredential: typeof window.database.credentials.delete;
+let removeCredentialsOfAccount: typeof window.database.credentials.deleteForAccount;
+let getCredentials: typeof window.database.credentials.getAll;
+let getCredentialsForIdentity: typeof window.database.credentials.getForIdentity;
+let getCredentialsOfAccount: typeof window.database.credentials.getForAccount;
+let getNextCredentialNumber: typeof window.database.credentials.getNextNumber;
+let updateCredentialIndex: typeof window.database.credentials.updateIndex;
+let updateCredential: typeof window.database.credentials.update;
+let hasDuplicateWalletId: typeof window.database.credentials.hasDuplicateWalletId;
+let hasExistingCredential: typeof window.database.credentials.hasExistingCredential;
 
-export const {
-    insert: insertCredential,
-    delete: removeCredential,
-    deleteForAccount: removeCredentialsOfAccount,
-    getAll: getCredentials,
-    getForIdentity: getCredentialsForIdentity,
-    getForAccount: getCredentialsOfAccount,
-    getNextNumber: getNextCredentialNumber,
-    updateIndex: updateCredentialIndex,
-    update: updateCredential,
+(async () => {
+    await waitForPreloadReady();
+    ({
+        insert: insertCredential,
+        delete: removeCredential,
+        deleteForAccount: removeCredentialsOfAccount,
+        getAll: getCredentials,
+        getForIdentity: getCredentialsForIdentity,
+        getForAccount: getCredentialsOfAccount,
+        getNextNumber: getNextCredentialNumber,
+        updateIndex: updateCredentialIndex,
+        update: updateCredential,
+        hasDuplicateWalletId,
+        hasExistingCredential
+    } = window.database.credentials);
+})();
+
+export {
+    insertCredential,
+    removeCredential,
+    removeCredentialsOfAccount,
+    getCredentials,
+    getCredentialsForIdentity,
+    getCredentialsOfAccount,
+    getNextCredentialNumber,
+    updateCredentialIndex,
+    updateCredential,
     hasDuplicateWalletId,
-    hasExistingCredential,
-} = window.database.credentials;
+    hasExistingCredential
+};
