74145df9a337576a429df15d7c6bfe8b664a3fb4
Mani <msinghster@gmail.com>
2025-04-30 04:39:07 -0600

    Added Comment on files that uses async

--- app/database/AccountDao.ts
+++ app/database/AccountDao.ts
@@ -1,4 +1,30 @@
 /* eslint-disable import/no-mutable-exports */
+
+/**
+ * NOTE: These functions are loaded from the preload script, but they are not available right away.
+ *
+ * We have to wait for the preload to finish before we can use `window.database`.
+ * However, if we use `await` at the top level, TypeScript throws the following error during Jest tests:
+ *
+ *   error TS1378: Top-level 'await' expressions are only allowed when the 'module' option is set to
+ *   'es2022', 'esnext', 'system', 'node16', 'nodenext', or 'preserve', and the 'target' option is set to 'es2017' or higher.
+ *
+ * Even though our `tsconfig.json` already includes:
+ *
+ *     "target": "ES2020",
+ *     "module": "ESNext",
+ *
+ * this error still appears when running tests
+ *
+ * To avoid changing the whole app setup or Jest environment, we use this workaround:
+ * 
+ * 1. Declare the variables (`let ...`)
+ * 2. Wait for preload inside an async IIFE, then assign the values
+ * 3. Export the variables so other parts of the app can use them
+ *
+ * This ensures we don’t access uninitialized APIs and keeps both the app and tests working properly.
+ */
+
 import { Account } from '../utils/types';
 import waitForPreloadReady from '../utils/preloadReady';
 
