afa5a874023d5998a24156888c7b2349036f9f1d
Doris Benda <13039169+DOBEN@users.noreply.github.com>
2025-08-07 18:19:14 +0300

    Fill form

--- app/pages/multisig/updates/CreatePLT/CreatePltParameters.tsx
+++ app/pages/multisig/updates/CreatePLT/CreatePltParameters.tsx
@@ -2,31 +2,51 @@ import React from 'react';
 import { EqualRecord } from '~/utils/types';
 import { UpdateProps } from '~/utils/transactionTypes';
 import Form from '~/components/Form/';
-import { mustBeAnInteger, requiredMessage, enterHere } from '../common/util';
+import { mustBeAnInteger, requiredMessage } from '../common/util';
 
 export interface UpdateCreatePltParametersFields {
+    tokenId: string;
+    name: string;
+    moduleRef: string,
+    metadataUrl: string;
+    metadataHash: string | undefined;
+    governanceAccount: string,
     decimals: number,
     initialSupply: bigint,
+    allowList: boolean;
+    denyList: boolean;
+    mintable: boolean;
+    burnable: boolean;
 }
 
 const fieldNames: EqualRecord<UpdateCreatePltParametersFields> = {
+    tokenId: 'tokenId',
+    name: 'name',
+    moduleRef: 'moduleRef',
+    metadataUrl: 'metadataUrl',
+    metadataHash: 'metadataHash',
+    governanceAccount: 'governanceAccount',
     decimals: 'decimals',
     initialSupply: 'initialSupply',
+    allowList: 'allowList',
+    denyList: 'denyList',
+    mintable: 'mintable',
+    burnable: 'burnable'
 };
 
 export const fieldDisplays = {
-    // TODO:
-    // tokenId     
-    // tokenModule 
-    // governanceAccount 
-    // name   
-    // metadata     
-    // allowList   
-    // denyList
-    // mintable
-    // burnable
+    tokenId: 'TokenId',
+    name: 'Name',
+    moduleRef: 'ModuleRef',
+    metadataUrl: 'MetadataUrl',
+    metadataHash: 'MetadataHash',
+    governanceAccount: 'GovernanceAccount',
     decimals: 'Decimals',
     initialSupply: 'InitialSupply',
+    allowList: 'AllowList',
+    denyList: 'DenyList',
+    mintable: 'Mintable',
+    burnable: 'Burnable',
 };
 
 /**
@@ -35,15 +55,62 @@ export const fieldDisplays = {
 export default function CreatePltParameters({
 }: UpdateProps): JSX.Element | null {
     return (
-        <div>           
+        <div>
+            <Form.Input
+                className="body2 mB20"
+                name="tokenId"
+                label="Token ID"
+                maxLength={128}
+                pattern="^[a-zA-Z0-9.%-]{1,128}$"
+                placeholder="Alphanumeric and . % - (max 128 chars)"
+                rules={{
+                    required: requiredMessage(fieldDisplays.tokenId),
+                    validate: (value: string) => {
+                        const cleaned = value.replace(/[^a-zA-Z0-9.%-]/g, '');
+                        if (cleaned.length > 128) return "Must be 128 characters or less.";
+                        if (value !== cleaned) return "Only letters, numbers, '.', '%', and '-' are allowed.";
+                        return true;
+                    }
+                }}
+            />
+            < Form.Input
+                className="body2 mB20"
+                name="name"
+                label="Name"
+                placeholder="Token Name"
+                rules={{ required: requiredMessage(fieldDisplays.name), }}
+            />
+            <Form.Input
+                className="body2 mB20"
+                name="moduleRef"
+                label="Token Module Reference"
+                defaultValue="5c5c2645db84a7026d78f2501740f60a8ccb8fae5c166dc2428077fd9a699a4a"
+            />
+            <Form.Input
+                className="body2 mB20"
+                name="metadataUrl"
+                label="Metadata URL"
+                placeholder="https://tokenWebsite.com"
+                rules={{ required: requiredMessage(fieldDisplays.metadataUrl), }}
+            />
+            <Form.Input
+                className="body2 mB20"
+                name="metadataHash"
+                label="Metadata Hash"
+                placeholder="b27a46456f5d3c089f7ad76bbbc3525ef277532b131ffadfc2565094c4b5133a"
+            />
+            <Form.Input
+                className="body2 mB20"
+                name="governanceAccount"
+                label="Governance Account"
+                placeholder="4BTFaHx8CioLi8Xe7YiimpAK1oQMkbx5Wj6B8N7d7NXgmLvEZs"
+                rules={{ required: requiredMessage(fieldDisplays.governanceAccount) }}
+            />
             <Form.Input
                 className="body2 mB20"
                 name={fieldNames.decimals}
-                defaultValue={
-                    0
-                }
                 label={`${fieldDisplays.decimals}`}
-                placeholder={enterHere(fieldDisplays.decimals)}
+                placeholder="0"
                 rules={{
                     required: requiredMessage(fieldDisplays.decimals),
                     min: {
@@ -58,11 +125,8 @@ export default function CreatePltParameters({
             <Form.Input
                 className="body2 mB20"
                 name={fieldNames.initialSupply}
-                defaultValue={
-                    0
-                }
+                placeholder="0"
                 label={`${fieldDisplays.initialSupply}`}
-                placeholder={enterHere(fieldDisplays.initialSupply)}
                 rules={{
                     required: requiredMessage(fieldDisplays.initialSupply),
                     min: {
@@ -74,6 +138,18 @@ export default function CreatePltParameters({
                     },
                 }}
             />
+            <Form.Checkbox name="allowList" className="body2 mB20" >
+                Has allow list
+            </Form.Checkbox>
+            <Form.Checkbox name="denyList" className="body2 mB20" >
+                Has deny list
+            </Form.Checkbox>
+            <Form.Checkbox name="mintable" className="body2 mB20">
+                Mintable
+            </Form.Checkbox>
+            <Form.Checkbox name="burnable" className="body2 mB20">
+                Burnable
+            </Form.Checkbox>
         </div>
     );
 }
--- app/pages/multisig/updates/CreatePLT/CreatePltParametersView.tsx
+++ app/pages/multisig/updates/CreatePLT/CreatePltParametersView.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CreatePLTPayload } from '@concordium/web-sdk/plt';
+import { Cbor, CreatePLTPayload, TokenInitializationParameters } from '@concordium/web-sdk/plt';
 
 import Loading from '~/cross-app-components/Loading';
 import withChainData, { ChainData } from '~/utils/withChainData';
@@ -14,22 +14,90 @@ interface Props extends ChainData {
  * Displays an overview of an update create PLT transaction transaction payload.
  */
 export default withChainData(function CreatePltParametersView({
-    createPltParameters: { decimals},
+    createPltParameters: { decimals, tokenId, moduleRef, initializationParameters },
     consensusStatus,
 }: Props) {
     if (!consensusStatus) {
         return <Loading inline />;
     }
 
+    // TODO: decoding problem as `hasOwn` function not available
+    let test: TokenInitializationParameters = Cbor.decode(initializationParameters, 'TokenInitializationParameters');
+    console.log(test)
     return (
         <>
             <div>
-                {/* TODO: display the other fields */}
+                <Label className="mB5">
+                    {fieldDisplays.tokenId}:
+                </Label>
+                <div className="body3 mono mB20">{tokenId.toString()}</div>
+            </div>
+            {/* <div>
+                <Label className="mB5">
+                    {fieldDisplays.name}:
+                </Label>
+                <div className="body3 mono mB20">{name}</div>
+            </div> */}
+            <div>
+                <Label className="mB5">
+                    {fieldDisplays.moduleRef}:
+                </Label>
+                <div className="body3 mono mB20">{moduleRef.toString()}</div>
+            </div>
+            {/* <div>
+                   <Label className="mB5">
+                    {fieldDisplays.metadataUrl}:
+                </Label>
+                <div className="body3 mono mB20">{metadataUrl}</div>
+            </div>
+             <div>
+                  <Label className="mB5">
+                    {fieldDisplays.metadataHash}:
+                </Label>
+                <div className="body3 mono mB20">{metadataHash}</div>
+            </div>
+             <div>
+                <Label className="mB5">
+                    {fieldDisplays.governanceAccount}:
+                </Label>
+                <div className="body3 mono mB20">{governanceAccount}</div>
+            </div> */}
+            <div>
                 <Label className="mB5">
                     {fieldDisplays.decimals}:
                 </Label>
                 <div className="body3 mono mB20">{decimals.toString()}</div>
             </div>
+            {/* <div>
+               <Label className="mB5">
+                    {fieldDisplays.initialSupply}:
+                </Label>
+                <div className="body3 mono mB20">{initialSupply.toString()}</div>
+            </div>
+             <div>
+                <Label className="mB5">
+                    {fieldDisplays.allowList}:
+                </Label>
+                <div className="body3 mono mB20">{allowList}</div>
+            </div>
+             <div>
+                <Label className="mB5">
+                    {fieldDisplays.denyList}:
+                </Label>
+                <div className="body3 mono mB20">{denyList.toString()}</div>
+            </div>
+             <div>
+                <Label className="mB5">
+                    {fieldDisplays.mintable}:
+                </Label>
+                <div className="body3 mono mB20">{mintable.toString()}</div>
+            </div>
+             <div>
+                <Label className="mB5">
+                    {fieldDisplays.burnable}:
+                </Label>
+                <div className="body3 mono mB20">{burnable.toString()}</div>
+            </div> */}
         </>
     );
 });
